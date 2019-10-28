23a1ccd57dbe2346cb06bfbfe35a1ac66980199b
Christian <s134801@student.dtu.dk>
2019-10-28 17:37:45 +0100

    unit test for array utils

---
+++ utilities/UnitTest.jl
@@ -0,0 +1,18 @@
+#!/usr/bin/env julia
+module UnitTest
+using Test
+include("ArrayUtils.jl")
+using .ArrayUtils
+
+v = rand(3)
+
+@testset "ArrayUtils" begin
+	@test hcatpad(v) == hcat(v)
+	@test hcatpad([v, v]) == hcat(v, v)
+	@test size(reorder(rand(4,4), [3,4,1,2])) == (4,4)
+	@test_logs (:warn, "Indexing outside range of shortest axis.") reorder(rand(5,4), [3,4,1,2,5])
+	@test_throws BoundsError reorder(rand(4,4), [3,4,1,2,5])
+	@test size(reorder(rand(4,4), [3,4,1])) == (3,3)
+end
+
+end;
