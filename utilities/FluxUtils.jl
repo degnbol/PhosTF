27847d80704a7b9c3e1bde5cc8147828f338c2c9
Christian <s134801@student.dtu.dk>
2019-10-28 17:40:42 +0100

    extended printfmt to handle tracked values

--- utilities/FluxUtils.jl
+++ utilities/FluxUtils.jl
@@ -2,7 +2,9 @@
 module FluxUtils
 using LinearAlgebra
 using Flux
+using Flux: Tracker
 import Base: *
+import Formatting
 
 export zerodiag
 export random_weight
@@ -15,5 +17,13 @@ random_weight(n::Integer, m::Integer) = randn(n, m) ./ √(n*m)
 # overloading missing multiplication signature that gives ambiguity.
 # we are selecting the tracker multiplication over the LinearAlgebra one since we obviously want tracking.
 *(x::LinearAlgebra.Diagonal, y::Tracker.TrackedArray{T,2,A} where A where T) = Tracker.track(*, x, y)
+
+
+Formatting.printfmt(fe::AbstractString, args::Tracker.TrackedReal...) = Formatting.printfmt(fe, [Tracker.data(v) for v in args]...)
+function Formatting.printfmt(prec::Integer, args::Tracker.TrackedReal...; sep="\t")
+    Formatting.printfmt(join(["{:.$(prec)f}" for _ in args], sep), args...)
+end
+Formatting.printfmtln(fe::AbstractString, args::Tracker.TrackedReal...) = (Formatting.printfmt(fe, args...); println())
+Formatting.printfmtln(prec::Integer, args::Tracker.TrackedReal...; sep="\t") = (Formatting.printfmt(prec, args...; sep=sep); println())
 
 end;
