19a8ba963bb0032300c2dffcb9f8b3194d93e2d5
Christian <s134801@student.dtu.dk>
2019-11-18 14:01:58 +0100

    prepare for large scale test with 100 nodes

--- test.jl
+++ test.jl
@@ -1,43 +1,62 @@
 #!/usr/bin/env julia
-cd("testdata/pres18c")
-cd("../data_cas"); nₜ, nₚ = 3, 3
-cd("../pres18c"); nₜ, nₚ = 3, 3
-cd("../simi"); nₜ, nₚ = 2, 3
-cd("../simi_neg"); nₜ, nₚ = 2, 3
-cd("../cycl"); nₜ, nₚ = 2, 2
-cd("../cycl_neg"); nₜ, nₚ = 2, 2
+# cd("testdata/pres18c")
+# cd("../data_cas"); nₜ, nₚ = 3, 3
+# cd("../pres18c"); nₜ, nₚ = 3, 3
+# cd("../simi"); nₜ, nₚ = 2, 3
+# cd("../simi_neg"); nₜ, nₚ = 2, 3
+# cd("../cycl"); nₜ, nₚ = 2, 2
+# cd("../cycl_neg"); nₜ, nₚ = 2, 2
 
-
+include("PKTFX.jl")
 tryrm(fname) = try rm(fname) catch IOError end
 # include("../../utilities/UnitTest.jl")
-include("../../PKTFX.jl")
 
-# PKTFX.network()
-PKTFX.xgmml(o="net.xgmml")
-
-PKTFX.simulate()
-PKTFX.simulate(1; r0="sim_r.mat", p0="sim_p.mat", phi0="sim_phi.mat")
-PKTFX.simulate(2; r0="sim_r.mat", p0="sim_p.mat", phi0="sim_phi.mat")
-PKTFX.simulate(3; r0="sim_r.mat", p0="sim_p.mat", phi0="sim_phi.mat")
-PKTFX.simulate(4; r0="sim_r.mat", p0="sim_p.mat", phi0="sim_phi.mat")
-
-PKTFX.steadystate()
-PKTFX.xgmml(X=["steady_r.mat", "steady_p.mat", "steady_phi.mat"], o="steady.xgmml")
-
-tryrm("X_sim.mat"); PKTFX.logFC(o="X_sim.mat")
-tryrm("sim.xgmml"); PKTFX.xgmml(X="X_sim.mat", o="sim.xgmml")
-
-PKTFX.T("X_sim.mat", "T_sim.mat")
-PKTFX.thres("T_sim.mat", "T_sim_thres.mat")
-PKTFX.xgmml("T_sim_thres.mat", nₜ, nₚ; o="T.xgmml")
-
-tryrm("WT_infer.mat"); tryrm("WP_infer.mat"); tryrm("WT_infer_thres.mat"); tryrm("WP_infer_thres.mat"); tryrm("sim_infer.xgmml")
-PKTFX.infer("X_sim.mat", nₜ, nₚ; lambda=.1, epochs=20000)
-PKTFX.thres("WT_infer.mat", "WT_infer_thres.mat")
-PKTFX.thres("WP_infer.mat", "WP_infer_thres.mat")
-PKTFX.xgmml("WT_infer_thres.mat", "WP_infer_thres.mat", o="sim_infer.xgmml")
+# go to dream folder
+# cd("/Users/christian/GoogleDrev/PKTFX/data/dream")
 
 
+nₚₖ, nₚₚ = 20, 5 # ≈ (144 - 30) / (144+266), 30 / (144+266)
 
-# now open xgmml files in cytoscape and have a look
+for lossfun ∈ ["LB", "LT", "L1"]
+    for lambda ∈ [1., .1]
+        for gen_fun ∈ ["100", "50"]
+            cd(lossfun * lpad(Int64(lambda*10), 2, '0') * "_" * gen_fun)
+            for net ∈ 1:5
+                for sample ∈ 1:2
+                    cd("$(net)_$(sample)")
 
+                    PKTFX.W("goldstandard.mat", nₚₖ, nₚₚ, gen_fun)
+                    PKTFX.correct()  # there should not be any corrections to make
+                    n, nₜ = size(PKTFX.loaddlm("WT.mat"))
+                    _, nₚ = size(PKTFX.loaddlm("WP.mat"))
+                    
+                    PKTFX.network()
+                    # PKTFX.xgmml(o="net.xgmml")
+
+                    # PKTFX.simulate()
+                    # PKTFX.simulate(1; r0="sim_r.mat", p0="sim_p.mat", phi0="sim_phi.mat")
+
+                    # PKTFX.steadystate()
+                    # PKTFX.xgmml(X=["steady_r.mat", "steady_p.mat", "steady_phi.mat"], o="steady.xgmml")
+
+                    tryrm("X_sim.mat"); PKTFX.logFC(o="X_sim.mat")
+                    # tryrm("sim.xgmml"); PKTFX.xgmml(X="X_sim.mat", o="sim.xgmml")
+
+                    # PKTFX.T("X_sim.mat", "T_sim.mat")
+                    # PKTFX.thres("T_sim.mat", "T_sim_thres.mat")
+                    # PKTFX.xgmml("T_sim_thres.mat", nₜ, nₚ; o="T.xgmml")
+
+                    tryrm("WT_infer.mat"); tryrm("WP_infer.mat"); tryrm("WT_infer_thres.mat")
+                    tryrm("WP_infer_thres.mat"); tryrm("infer.xgmml")
+                    PKTFX.infer("X_sim.mat", nₜ, nₚ; lossfun=lossfun, lambda=lambda, epochs=10000)
+                    PKTFX.thres("WT_infer.mat", "WT_infer_thres.mat")
+                    PKTFX.thres("WP_infer.mat", "WP_infer_thres.mat")
+                    # PKTFX.xgmml("WT_infer_thres.mat", "WP_infer_thres.mat", o="infer.xgmml")
+            
+                    # now open xgmml files in cytoscape and have a look
+                    cd("../..")
+                end
+            end
+        end
+    end
+end
