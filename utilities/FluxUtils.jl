4e9ab52ae36efd731f2f7c4131b775bf00bf43f0
Christian <s134801@student.dtu.dk>
2019-11-19 16:30:49 +0100

    seems my quickfix is not needed anymore? maybe the bug was fixed

--- utilities/FluxUtils.jl
+++ utilities/FluxUtils.jl
@@ -16,7 +16,7 @@ random_weight(n::Integer, m::Integer) = randn(n, m) ./ √(n*m)
 
 # overloading missing multiplication signature that gives ambiguity.
 # we are selecting the tracker multiplication over the LinearAlgebra one since we obviously want tracking.
-*(x::LinearAlgebra.Diagonal, y::Tracker.TrackedArray{T,2,A} where A where T) = Tracker.track(*, x, y)
+# *(x::LinearAlgebra.Diagonal, y::Tracker.TrackedMatrix) = Tracker.track(*, x, y)
 
 
 Formatting.printfmt(fe::AbstractString, args::Tracker.TrackedReal...) = Formatting.printfmt(fe, [Tracker.data(v) for v in args]...)
