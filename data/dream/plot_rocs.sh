0ab1ab970841d723821792322df7d4524b6d1ed8
Christian <s134801@student.dtu.dk>
2019-11-20 11:07:36 +0100

    plot rocs for all 25 runs, using sim folder pink

--- data/dream/plot_rocs.sh
+++ data/dream/plot_rocs.sh
@@ -1,2 +1,2 @@
-for dir in lambda*; do rocauc.py $dir/?_?/WT.mat $dir/?_?/WT_infer_thres.mat -title $dir -out $dir/WT.pdf > $dir/auc.log; done
-for dir in lambda*; do rocauc.py $dir/?_?/WP.mat $dir/?_?/WP_infer_thres.mat -title $dir -out $dir/WP.pdf > $dir/auc.log; done
+for dir in lambda*; do rocauc.py $dir/?_?/sim/WT.mat $dir/?_?/WT_infer_thres.mat -title $dir -out $dir/WT.pdf > $dir/auc.log; done
+for dir in lambda*; do rocauc.py $dir/?_?/sim/WP.mat $dir/?_?/WP_infer_thres.mat -title $dir -out $dir/WP.pdf > $dir/auc.log; done
