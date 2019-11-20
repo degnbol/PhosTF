0bc0ad4aa4b0ea65bd6b12a6995a26ddd3bfd967
Christian <s134801@student.dtu.dk>
2019-11-20 11:06:31 +0100

    plot rocs for all 25 runs

--- data/dream/plot_rocs.sh
+++ data/dream/plot_rocs.sh
@@ -1,2 +1,2 @@
-for dir in L*; do rocauc.py $dir/?_1/WT.mat $dir/?_1/WT_infer_thres.mat -title $dir -out $dir/WT.pdf > $dir/auc.log; done
-for dir in L*; do rocauc.py $dir/?_1/WP.mat $dir/?_1/WP_infer_thres.mat -title $dir -out $dir/WP.pdf > $dir/auc.log; done
+for dir in lambda*; do rocauc.py $dir/?_?/WT.mat $dir/?_?/WT_infer_thres.mat -title $dir -out $dir/WT.pdf > $dir/auc.log; done
+for dir in lambda*; do rocauc.py $dir/?_?/WP.mat $dir/?_?/WP_infer_thres.mat -title $dir -out $dir/WP.pdf > $dir/auc.log; done
