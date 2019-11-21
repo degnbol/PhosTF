17f84fde3a5247f330bfcbb0b5367295873a0c16
Christian <s134801@student.dtu.dk>
2019-11-21 16:52:57 +0100

    only 1000 epochs instead of 10000 to correct edges when using PK vs PP info

--- dream_test.jl
+++ dream_test.jl
@@ -15,8 +15,10 @@ for net ∈ 1:5
     n, nₜ = size(PKTFX.loaddlm("sim/WT.mat"))
     _, nₚ = size(PKTFX.loaddlm("sim/WP.mat"))
 
+    tryrm("WT_infer.mat"); tryrm("WP_infer.mat"); tryrm("WT_infer_thres.mat"); tryrm("WP_infer_thres.mat")
+    tryrm("infer.xgmml")
     PKTFX.infer("sim/X_sim.mat", nₜ, nₚ; lambda_W=lambda_W, lambda_B=lambda_B, epochs=10000)
-    PKTFX.infer("sim/X_sim.mat", nₜ, nₚ, "WT_infer_V.mat", "WP_infer_V.mat"; lambda_W=lambda_W, lambda_B=lambda_B, epochs=10000, PKPP="sim/PKPP.mat", WT="WT_infer.mat", WP="WP_infer.mat")
+    PKTFX.infer("sim/X_sim.mat", nₜ, nₚ, "WT_infer_V.mat", "WP_infer_V.mat"; lambda_W=lambda_W, lambda_B=lambda_B, epochs=1000, PKPP="sim/PKPP.mat", WT="WT_infer.mat", WP="WP_infer.mat")
     PKTFX.thres("WT_infer_V.mat", "WT_infer_thres.mat"; thres=1e-4)
     PKTFX.thres("WP_infer_V.mat", "WP_infer_thres.mat"; thres=1e-4)
     PKTFX.correct("WT_infer_thres.mat", "WP_infer_thres.mat")
