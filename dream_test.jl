a73286a239f7e3a24ed69d8ce131cbfd631e17de
Christian <s134801@student.dtu.dk>
2019-11-22 11:12:26 +0100

    fewer epochs to save time for dream tests

--- dream_test.jl
+++ dream_test.jl
@@ -17,7 +17,7 @@ for net ∈ 1:5
 
     tryrm("WT_infer.mat"); tryrm("WP_infer.mat"); tryrm("WT_infer_thres.mat"); tryrm("WP_infer_thres.mat")
     tryrm("infer.xgmml")
-    PKTFX.infer("sim/X_sim.mat", nₜ, nₚ; lambda_W=lambda_W, lambda_B=lambda_B, epochs=10000)
+    PKTFX.infer("sim/X_sim.mat", nₜ, nₚ; lambda_W=lambda_W, lambda_B=lambda_B, epochs=5000)
     PKTFX.infer("sim/X_sim.mat", nₜ, nₚ, "WT_infer_V.mat", "WP_infer_V.mat"; lambda_W=lambda_W, lambda_B=lambda_B, epochs=1000, PKPP="sim/PKPP.mat", WT="WT_infer.mat", WP="WP_infer.mat")
     PKTFX.thres("WT_infer_V.mat", "WT_infer_thres.mat"; thres=1e-4)
     PKTFX.thres("WP_infer_V.mat", "WP_infer_thres.mat"; thres=1e-4)
