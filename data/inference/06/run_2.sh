2389ac80178dbf90a9d128575871731469db9cfa
Christian Degnbol Madsen <cdegma@dtu.dk>
2019-12-11 16:12:44 +0100

    results where OE was reduced by 6 by mistake. 600 epochs

--- data/inference/06/run_2.sh
+++ data/inference/06/run_2.sh
@@ -1,1 +1,3 @@
+export JULIA_NUM_THREADS=12
+export OPENBLAS_NUM_THREADS=12
 ../../../PKTFX.jl infer ../../pertubation/logFC.mat 272 153 --epochs 500 --J ../../pertubation/J.mat --WT WT_infer_1.mat --WP WP_infer_1.mat
