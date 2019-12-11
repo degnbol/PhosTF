8516be6dc8f501e41b64899f9f6b0ada9c245eb5
Christian Degnbol Madsen <cdegma@dtu.dk>
2019-12-11 16:13:58 +0100

    adding extra cores

--- data/inference/05/run_2.sh
+++ data/inference/05/run_2.sh
@@ -1,1 +1,3 @@
+export JULIA_NUM_THREADS=12
+export OPENBLAS_NUM_THREADS=12
 ../../../PKTFX.jl infer ../../pertubation/logFC.mat 272 153 --epochs 500 --WT_prior ../../pertubation/WT_prior.mat --J ../../pertubation/J.mat --WT WT_infer_1.mat --WP WP_infer_1.mat
--- data/inference/10/run_2.sh
+++ data/inference/10/run_2.sh
@@ -1,1 +1,3 @@
- --WT WT_infer_1.mat --WP WP_infer_1.mat
+export JULIA_NUM_THREADS=12
+export OPENBLAS_NUM_THREADS=12
+../../../PKTFX.jl infer ../../pertubation/logFC_inner.mat 272 153 --epochs 500 --WT_prior ../../pertubation/WT_prior.mat --J ../../pertubation/J_inner.mat --WT WT_infer_1.mat --WP WP_infer_1.mat
