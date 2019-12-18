039c693061bc0828097a863d96b3dfdb5d134e90
Christian <s134801@student.dtu.dk>
2019-12-18 11:17:30 +0100

    look at the goncalves data

---
+++ data/processed/goncalves_2017/process_goncalves.R
@@ -0,0 +1,6 @@
+
+setwd("/Users/christian/GoogleDrev/PKTFX/data/processed/goncalves_2017")
+
+
+KOKP = read.table("KP_KO_KP.tsv", row.names=1, sep="\t", header=T, quote="")
+KOTF = read.table("KP_KO_TF.tsv", row.names=1, sep="\t", header=T, quote="")
