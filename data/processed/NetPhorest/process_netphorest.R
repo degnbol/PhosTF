677672585e41bcfd22ddcb0f1ca0d3b142394f70
Christian <s134801@student.dtu.dk>
2019-12-18 11:16:42 +0100

    predictions run with phosphorylation inference methods and aggregated for each unique regulator/target pair in the dataset.

---
+++ data/processed/NetPhorest/process_netphorest.R
@@ -0,0 +1,13 @@
+
+setwd("/Users/christian/GoogleDrev/PKTFX/data/processed/NetPhorest")
+
+posteriors = read.table("netphorest_yeast_col189.tsv", sep="\t", header=T, quote="")
+posteriors_agg = aggregate(Posterior ~ ., data=posteriors, max)
+scores = data.frame(KP=posteriors_agg$KP, Target=posteriors_agg$Name, Score=posteriors_agg$Posterior)
+write.table(scores, file="scores.tsv", sep="\t", quote=F, row.names=F)
+# scores = read.table("scores.tsv", sep="\t", quote="", header=T)
+
+plot(density(scores$Score))
+hist(scores$Score, breaks=30)
+sum(scores$Score > 0.3)
+sum(scores$Score < 0.03)
