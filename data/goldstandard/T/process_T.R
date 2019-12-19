84389439d6d570df0cea5ceb638a1bcadfe377ca
Christian <s134801@student.dtu.dk>
2019-12-19 17:06:27 +0100

    added balaji 2008 edges but they contributed very little to what is already collectd

--- data/goldstandard/T/process_T.R
+++ data/goldstandard/T/process_T.R
@@ -2,6 +2,7 @@
 setwd("/Users/christian/GoogleDrev/PKTFX/data/goldstandard/T")
 
 balaji = read.table("balaji.tsv", sep="\t", col.names=c("TF", "Target"))
+balaji_2008 = read.table("balaji_2008.tsv", sep="\t", col.names=c("TF", "Target"))
 harbison = read.table("harbison.tsv", sep="\t", col.names=c("TF", "Target"))
 horak = read.table("horak.tsv", sep="\t", col.names=c("TF", "Target"))
 workman = read.table("workman.tsv", sep="\t", header=T)
@@ -12,7 +13,7 @@ STRING$Mode = NA
 STRING$Mode[STRING$activation > STRING$inhibition | is.na(STRING$inhibition)] = "activator"
 STRING$Mode[STRING$activation < STRING$inhibition | is.na(STRING$activation)] = "inhibitor"
 
-edges = unique(rbind(balaji, harbison, horak, workman, yeastract[,1:2]))
+edges = unique(rbind(balaji, balaji_2008, harbison, horak, workman, yeastract[,1:2]))
 # add regulation mode
 edges = merge(edges, yeastract, all.x=T)
 edges = merge(edges, STRING[,c(1,2,5)], all.x=T)
