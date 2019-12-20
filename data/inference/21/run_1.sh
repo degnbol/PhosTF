bacced2801f8851cb6085b35d96675eb604b37fe
Christian Degnbol Madsen <cdegma@dtu.dk>
2019-12-20 15:24:41 +0100

    inference with lambda=.01, lambdaW=0.0, no prior 200 epochs, outer dataset

---
+++ data/inference/21/run_1.sh
@@ -0,0 +1,3 @@
+export JULIA_NUM_THREADS=12
+export OPENBLAS_NUM_THREADS=12
+../../../PKTFX.jl infer ../../perturbation/logFC.mat 272 153 --epochs 200 --J ../../perturbation/J.mat --lambda 0.01 --lambdaW 0.0 | tee infer_1.log
