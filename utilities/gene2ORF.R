2308b16b7665cd3b23abee14e54dff7e424d310b
Christian <s134801@student.dtu.dk>
2020-01-02 11:18:25 +0100

    convert ignoring case

--- utilities/gene2ORF.R
+++ utilities/gene2ORF.R
@@ -1,12 +1,18 @@
 #!/usr/bin/env Rscript
 
 # functions
+simplify = function(x) gsub(" ", "", gsub("_", "", toupper(trimws(x))))
+# same as match but ignore case and _ and " "
+match_ignore = function(x, table) match(simplify(x), simplify(table))
 # assuming ORF in column 1, and Gene in column 2
-convert_column = function(column) converter[match(trimws(column), converter[,2]),1]
-convert_table = function(gene.table) {
+convert_column = function(column, ignore=F) {
+    if (ignore) return(converter[match_ignore(column, converter[,2]),1])
+    else return(converter[match(column, converter[,2]),1])
+}
+convert_table = function(gene.table, ignore=F) {
     ORF.table = gene.table
     for (i in 1:ncol(ORF.table)) {
-        ORF.table[,i] = convert_column(gene.table[,i])
+        ORF.table[,i] = convert_column(gene.table[,i], ignore=ignore)
         # we use the original value when no conversion found
         no_conversion_idx = is.na(ORF.table[,i])
         ORF.table[no_conversion_idx,i] = gene.table[no_conversion_idx,i]
@@ -28,5 +34,5 @@ converter_fname = args[3]
 # main
 gene.table = read.table(infname, sep="\t", quote="", stringsAsFactors=F, colClasses="character")
 converter = read.table(converter_fname, sep="\t", quote="", stringsAsFactors=F)
-ORF.table = convert_table(gene.table)
+ORF.table = convert_table(convert_table(gene.table, ignore=F), ignore=T)
 write.table(ORF.table, file=outfname, row.names=F, col.names=F, quote=F, sep="\t")
