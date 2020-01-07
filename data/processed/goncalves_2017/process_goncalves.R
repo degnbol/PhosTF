1c7ebc8c9847c528ead53f9f6c3ca08174f9f92d
Christian <s134801@student.dtu.dk>
2020-01-07 18:36:11 +0100

    very basic analysis removed

--- data/processed/goncalves_2017/process_goncalves.R
+++
@@ -1,6 +0,0 @@
-
-setwd("~/cwd/data/processed/goncalves_2017")
-
-
-KOKP = read.table("KP_KO_KP.tsv", row.names=1, sep="\t", header=T, quote="")
-KOTF = read.table("KP_KO_TF.tsv", row.names=1, sep="\t", header=T, quote="")
