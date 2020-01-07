010909b617769bd677621c32140d8a082c56b461
Christian <s134801@student.dtu.dk>
2020-01-07 18:35:27 +0100

    split TF and PK in luscombe from the start

--- data/processed/luscombe_2010/process_luscombe.R
+++ data/processed/luscombe_2010/process_luscombe.R
@@ -1,27 +1,30 @@
+
 setwd("~/cwd/data/processed/luscombe_2010")
 
 # IMPORTANT that R does not check names since it will rename - to .
-TFKO = t(read.table("TF_KO_transpose.tsv", row.names=1, header=T, check.names=F, stringsAsFactors=F))
-TFKO_pval = t(read.table("TF_KO_pval_transpose.tsv", row.names=1, header=T, check.names=F, stringsAsFactors=F))
-
-# sanity check
-dim(TFKO)
-dim(TFKO_pval)
+KO = t(read.table("KO_transpose.tsv", row.names=1, header=T, check.names=F, stringsAsFactors=F))
+KO_pval = t(read.table("KO_pval_transpose.tsv", row.names=1, header=T, check.names=F, stringsAsFactors=F))
+# sanity checks
+dim(KO); dim(KO_pval)
+all(colnames(KO) == colnames(KO_pval))
 
 # convert name
 gene2ORF_table = read.table("../../name_conversion/gene2ORF.tsv", header=F, sep="\t", quote="", stringsAsFactors=F)
 gene2ORF = function(x) gene2ORF_table[match(x,gene2ORF_table[,2]),1]
 
-# first a sanity check
-all(colnames(TFKO) == colnames(TFKO_pval))
-
-names = toupper(colnames(TFKO))
+names = toupper(colnames(KO))
 ORFs = gene2ORF(names)
 ORFs[is.na(ORFs)] = names[is.na(ORFs)]
 # update colnames
-colnames(TFKO) = ORFs
-colnames(TFKO_pval) = ORFs
+colnames(KO) = ORFs
+colnames(KO_pval) = ORFs
 
-write.table(TFKO, file="TF_KO.tsv", quote=F, sep="\t", row.names=T)
-write.table(TFKO_pval, file="TF_KO_pval.tsv", quote=F, sep="\t", row.names=T)
+# there some MAPK kinases included
+kinase_cols = ORFs %in% c("YDR477W", "YLR113W", "YPL042C", "YGR040W", "YPR054W")
 
+write.table(KO[,kinase_cols], "PK_KO.tsv", quote=F, sep="\t", row.names=T)
+write.table(KO_pval[,kinase_cols], "PK_KO_pval.tsv", quote=F, sep="\t", row.names=T)
+write.table(KO[,!kinase_cols], "TF_KO.tsv", quote=F, sep="\t", row.names=T)
+write.table(KO_pval[,!kinase_cols], "TF_KO_pval.tsv", quote=F, sep="\t", row.names=T)
+
+# now go and add ORF to the beginning of file to match standard design of files
