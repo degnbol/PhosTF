30d10e44c576f5bff1803af99d72cea4a459245d
Christian <s134801@student.dtu.dk>
2020-01-13 16:55:30 +0100

    split harbison in YPD and other conds data instead of combine by min, so that we can use fisher's method later instead to combine pvals

--- data/processed/harbison_2004/process_harbison.R
+++ data/processed/harbison_2004/process_harbison.R
@@ -4,14 +4,17 @@ library(reshape2)
 setwd("~/cwd/data/processed/harbison_2004")
 
 YPD = as.matrix(read.table("YPD.tsv", header=T, row.names=1, sep="\t", check.names=F))
-YPD_lee_2002 = as.matrix(read.table("../lee_2002/YPD.tsv", header=T, row.names=1, sep="\t", check.names=F))
+YPD_lee = as.matrix(read.table("../lee_2002/YPD.tsv", header=T, row.names=1, sep="\t", check.names=F))
 conds = as.matrix(read.table("conds.tsv", header=T, row.names=1, sep="\t", check.names=F))
 
-YPD_melt = melt(YPD); colnames(YPD_melt) = c("gene", "TF", "Pval")
-YPD_lee_2002_melt = melt(YPD_lee_2002); colnames(YPD_lee_2002_melt) = c("gene", "TF", "Pval")
-conds_melt = melt(conds); colnames(conds_melt) = c("gene", "TF", "Pval")
+# there are perfect overlap among the values in YPD from harbison 2004 and lee 2002 for the same experiments so we take the unique
+YPD_melt = unique(na.omit(rbind(melt(YPD), melt(YPD_lee))))
+conds_melt = na.omit(melt(conds))
+colnames(YPD_melt) = c("Target", "TF", "Pval")
+colnames(conds_melt) = c("Target", "TF", "Pval")
 
-pvals = aggregate(Pval ~ ., data=rbind(YPD_melt, YPD_lee_2002_melt, conds_melt), min)
+# number of measurements
+nrow(YPD_melt)+nrow(conds_melt)
 
-write.table(pvals[,c(2,1,3)], file="TF_edges.tsv", quote=F, row.names=F, sep="\t")
-
+write.table(YPD_melt[,c(2,1,3)], "TF_edges_YPD.tsv", quote=F, row.names=F, sep="\t")
+write.table(conds_melt[,c(2,1,3)], "TF_edges_conds.tsv", quote=F, row.names=F, sep="\t")
