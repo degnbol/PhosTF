132c261bf87f0bbe6d4c37f129449e200f3cabbb
Christian <s134801@student.dtu.dk>
2020-01-21 14:41:16 +0100

    correct combinatorics of hyperparams and use .tsv file header directly to set variables.

--- data/inference/commands.sh
+++ data/inference/commands.sh
@@ -1,10 +1,12 @@
-# correct annoying line feed
+#!/usr/bin/env zsh
+nTF=$(cat ../network/TF.txt | wc -l | xargs)
+nKP=$(cat ../network/KP.txt | wc -l | xargs)
 cat hyperparams.tsv | tr -d '\r' > temp && mv temp hyperparams.tsv
-sed 1d hyperparams.tsv | tr '\t' ',' | while IFS=',' read folder X J epochs lambda lambdaW lambdaWT WT_mask WP_mask WT_reg WT WP trainWT linex; do
+sed 1d hyperparams.tsv | tr '\t' ',' | while IFS=',' read `head -n1 hyperparams.tsv`; do
 mkdir $folder
-echo "#!/usr/bin/env bash
+echo "#!/usr/bin/env zsh
 export JULIA_NUM_THREADS=12
 export OPENBLAS_NUM_THREADS=12
-../../../inference.jl \"$X\" 272 153 --J \"$J\" --epochs $epochs --lambda $lambda --lambdaW $lambdaW --lambdaWT $lambdaWT --WT-prior \"$WT_mask\" --WP-prior \"$WP_mask\" --WT-reg \"$WT_reg\" --WT \"$WT\" --WP \"$WP\" --trainWT $trainWT --linex $linex | tee infer_1.log" > $folder/commands.sh
+../../../inference.jl \"$X\" $nTF $nKP --J \"$J\" --epochs $epochs --lambda $lambda --lambdaW $lambdaW --lambdaWT $lambdaWT --WT-prior \"$WT_mask\" --WP-prior \"$WP_mask\" --WT-reg \"$WT_reg\" --WT \"$WT\" --WP \"$WP\" --trainWT $trainWT --linex $linex | tee infer_1.log" > $folder/commands.sh
 chmod 755 $folder/commands.sh
 done
