cd77fa010acc0de5bf83c352fc2c74c87eafd869
Christian <s134801@student.dtu.dk>
2020-01-24 13:20:58 +0100

    preparing to load wilcoxon inital results

--- data/network/KP_priors/KP2TF_p.R
+++ data/network/KP_priors/KP2TF_p.R
@@ -5,12 +5,14 @@ library(data.table)
 setwd("~/cwd/data/network/KP_priors")
 load("wilcoxon.RData")
 
+KP2TF = read.table("KP2TF_genesign.tsv", sep="\t", header=T)
+
 # the sign is currently for the PK KO effect 
+# so the sign in the file is KO_sign = - TF_sign * KP_sign => KP_sign = - KO_sign / TF_sign
+# which for TF_sign \in {-1,1} is the same as KP_sign = - KO_sign * TF_sign
 TFs$Mode[TFs$Mode == "activator"] = +1
 TFs$Mode[TFs$Mode == "repressor"] = -1
 
-
-KP_edges$sign[i] *= TFs$Mode[TFs$TF == TF]
-
+KP2TF$sign = - TFs$Mode[match(KP2TF$TF, TFs$TF)] * KP2TF$sign
 
 write.table(KP_edges, "KP2TF_p.tsv", sep="\t", quote=F, row.names=F)
