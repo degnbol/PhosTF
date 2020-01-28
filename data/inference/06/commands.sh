f9348fa36b8d5553a5fbfa96fb58da1c1009796d
Christian <s134801@student.dtu.dk>
2020-01-28 17:45:00 +0100

    use WP noise instead of WP as prior so values aren't exactly zero

--- data/inference/06/commands.sh
+++ data/inference/06/commands.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 export JULIA_NUM_THREADS=12
 export OPENBLAS_NUM_THREADS=12
-../../../inference.jl "~/cwd/data/perturbation/logFC_inner.mat" 231 199 --J "~/cwd/data/perturbation/J_inner.mat" --epochs 200 --lambda 0.1 --lambdaW 0 --lambdaWT false --WT-prior "~/cwd/data/network/WT_mask.mat" --WP-prior "" --WT-reg "" --WT "~/cwd/data/network/WT.mat" --WP "~/cwd/data/network/KP_priors/WP.mat" --trainWT true --linex false | tee infer_1.log
+../../../inference.jl "~/cwd/data/perturbation/logFC_inner.mat" 231 199 --J "~/cwd/data/perturbation/J_inner.mat" --epochs 200 --lambda 0.1 --lambdaW 0 --lambdaWT false --WT-prior "~/cwd/data/network/WT_mask.mat" --WP-prior "" --WT-reg "" --WT "~/cwd/data/network/WT.mat" --WP "~/cwd/data/network/KP_priors/WP_noise.mat" --trainWT true --linex false | tee infer_1.log
