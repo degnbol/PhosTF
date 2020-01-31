b18e9d32bacbc1c2b47c414f77f499d4266bebb8
Christian <s134801@student.dtu.dk>
2020-01-31 14:04:02 +0100

    TF priors own folder

--- data/network/TF_priors/putative_TF_edges.R
+++ data/network/TF_priors/putative_TF_edges.R
@@ -3,6 +3,10 @@ library(data.table)
 library(ggplot2)
 library(harmonicmeanp)
 
+# putative edge refers to an edge with significant p-value (<=0.05), where it is putative if the source of the edge is actually a TF.
+# this is settled in the ../nodes.R script which should be run next.
+
+
 # functions
 
 # combine pvals using fishers method https://en.wikipedia.org/wiki/Fisher%27s_method 
@@ -13,21 +17,21 @@ fisher.method = function(pvals) {
 }
 
 
-setwd("~/cwd/data/network")
+setwd("~/cwd/data/network/TF_priors")
 
 # read
-balaji_2006 = read.table("../processed/balaji_2006/TF_edges.tsv", sep="\t", header=T)[,1:2]
+balaji_2006 = read.table("~/cwd/data/processed/balaji_2006/TF_edges.tsv", sep="\t", header=T)[,1:2]
 # collected from harbison, lee 2002 (merged into harbison), and horak. so only use as upper bound for pval (pval<0.001)
-balaji_2008 = read.table("../processed/balaji_2008/TF_edges.tsv", sep="\t", header=T)
-harbison_YPD = read.table("../processed/harbison_2004/TF_edges_YPD.tsv", sep="\t", header=T)
-harbison_conds = read.table("../processed/harbison_2004/TF_edges_conds.tsv", sep="\t", header=T)
-horak = read.table("../processed/horak_2002/TF_edges.tsv", sep="\t", header=T)[,c(1,2,5)]; colnames(horak)[3] = "Pval"
-workman = read.table("../processed/workman_2006/TF_edges.tsv", sep="\t", header=T)[,c(1,2,4)]
-yeastract_binding = read.table("../processed/yeastract/binding_ORF.tsv", sep="\t", col.names=c("TF", "Target"))
+balaji_2008 = read.table("~/cwd/data/processed/balaji_2008/TF_edges.tsv", sep="\t", header=T)
+harbison_YPD = read.table("~/cwd/data/processed/harbison_2004/TF_edges_YPD.tsv", sep="\t", header=T)
+harbison_conds = read.table("~/cwd/data/processed/harbison_2004/TF_edges_conds.tsv", sep="\t", header=T)
+horak = read.table("~/cwd/data/processed/horak_2002/TF_edges.tsv", sep="\t", header=T)[,c(1,2,5)]; colnames(horak)[3] = "Pval"
+workman = read.table("~/cwd/data/processed/workman_2006/TF_edges.tsv", sep="\t", header=T)[,c(1,2,4)]
+yeastract_binding = read.table("~/cwd/data/processed/yeastract/binding_ORF.tsv", sep="\t", col.names=c("TF", "Target"))
 # expression data used to provide sign to edges, not to indicate presence of edges
-yeastract_expression = read.table("../processed/yeastract/expression_ORF.tsv", sep="\t", col.names=c("TF", "Target", "Mode"), na.strings="ambiguous")
+yeastract_expression = read.table("~/cwd/data/processed/yeastract/expression_ORF.tsv", sep="\t", col.names=c("TF", "Target", "Mode"), na.strings="ambiguous")
 yeastract_expression = na.omit(yeastract_expression)
-STRING = read.table("../processed/STRING/modes.tsv", sep="\t", header=T); colnames(STRING)[1] = "TF"
+STRING = read.table("~/cwd/data/processed/STRING/modes.tsv", sep="\t", header=T); colnames(STRING)[1] = "TF"
 # combine expression evidence, trust yeastract more than STRING since yeastract only has non-conflicting evidence, 
 # while STRING is a combined score and in cases where there is conflict it uses the highest scoring (activator or repressor)
 expression_evidence = rbind(yeastract_expression, STRING[!(paste(STRING$TF,STRING$Target) %in% paste(yeastract_expression$TF,yeastract_expression$Target)),])
@@ -52,12 +56,18 @@ all_edges = all_edges[,list(Pval=min(Pval)),by=list(TF,Target)]
 all_edges = merge(all_edges, expression_evidence, all.x=T)
 
 hist(all_edges$Pval, breaks=100)
+
+library(fdrtool)
+
 # remove all edges not supported in the data, we use 0.05 since it is the threshold that allows for the most edges.
 # we get ~100000 potential edges, which is still plenty
 sum(all_edges$Pval <= .05)
-edges = all_edges[all_edges$Pval <= .05,]
-
-write.table(edges, "TF_edges_putative.tsv", sep="\t", row.names=F, quote=F, na="")
+sum(p.adjust(all_edges$Pval, "BH") <= .05)
+fdr = fdrtool(all_edges$Pval, statistic="pvalue", plot=FALSE)
+sum(fdr$qval < .2)
+TF_edges = all_edges[all_edges$Pval <= .05,]
+# TF_edges = all_edges[fdr$qval <= .2,]  # uncomment and use this if you want to control FDR rate<0.2 rather than raw p<0.05
+write.table(TF_edges, "TF_edges_putative.tsv", sep="\t", row.names=F, quote=F, na="")
 
 
 
