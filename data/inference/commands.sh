6825a3752e730165d3afa0a54b2c3b8aee8e9025
Christian <s134801@student.dtu.dk>
2020-02-04 17:20:24 +0100

    use 6 instead of 12 threads per call

--- data/inference/commands.sh
+++ data/inference/commands.sh
@@ -5,8 +5,8 @@ cat hyperparams.tsv | tr -d '\r' > temp && mv temp hyperparams.tsv
 sed 1d hyperparams.tsv | tr '\t' ',' | while IFS=',' read `head -n1 hyperparams.tsv`; do
 mkdir $folder
 echo "#!/usr/bin/env zsh
-export JULIA_NUM_THREADS=12
-export OPENBLAS_NUM_THREADS=12
+export JULIA_NUM_THREADS=6
+export OPENBLAS_NUM_THREADS=6
 ../../../inference.jl \"$X\" $nTF $nKP --J \"$J\" --epochs $epochs --lambda $lambda --lambdaW $lambdaW --lambdaWT $lambdaWT --WT-prior \"$WT_mask\" --WP-prior \"$WP_mask\" --WT-reg \"$WT_reg\" --WT \"$WT\" --WP \"$WP\" --trainWT $trainWT --quadquad $quadquad | tee infer_1.log" > $folder/commands.sh
 chmod 755 $folder/commands.sh
 done
