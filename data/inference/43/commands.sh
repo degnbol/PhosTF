9ade3fd8898f170132ee95e318c5502616329aff
Christian <s134801@student.dtu.dk>
2020-02-11 16:48:48 +0100

    extra tests added

---
+++ data/inference/43/commands.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+export JULIA_NUM_THREADS=6
+export OPENBLAS_NUM_THREADS=6
+../../../inference.jl "~/cwd/data/perturbation/logFC_inner.mat" 231 199 --J "~/cwd/data/perturbation/J_inner.mat" --epochs 100 --lambda 0.1 --lambdaW 0 --lambdaWT false --WT-prior "~/cwd/data/network/WT_mask.mat" --WP-prior "~/cwd/data/network/KP_mask.mat" --WT-reg "" --WT "~/cwd/data/network/WT.mat" --WP "~/cwd/data/network/KP_priors/WP_noise.mat" --trainWT true --quadquad false | tee infer_1.log
