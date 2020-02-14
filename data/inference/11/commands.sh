0ec4c62b32423c62d9d872a12aade3886a76224d
Christian <s134801@student.dtu.dk>
2020-02-14 13:13:29 +0100

    use only 50 epochs to save time and since it matters very little to continue

--- data/inference/11/commands.sh
+++ data/inference/11/commands.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 export JULIA_NUM_THREADS=6
 export OPENBLAS_NUM_THREADS=6
-../../../inference.jl "~/cwd/data/perturbation/logFC_inner.mat" 231 199 --J "~/cwd/data/perturbation/J_inner.mat" --epochs 200 --lambda 0.1 --lambdaW 0 --lambdaWT true --WT-prior "" --WP-prior "~/cwd/data/network/KP_mask.mat" --WT-reg "~/cwd/data/network/WT_p.mat" --WT "~/cwd/data/network/WT.mat" --WP "~/cwd/data/network/KP_priors/WP_noise.mat" --trainWT true --quadquad true | tee infer_1.log
+../../../inference.jl "~/cwd/data/perturbation/logFC_inner.mat" 231 199 --J "~/cwd/data/perturbation/J_inner.mat" --epochs 50 --lambda 0.1 --lambdaW 0 --lambdaWT true --WT-prior "" --WP-prior "~/cwd/data/network/KP_mask.mat" --WT-reg "~/cwd/data/network/WT_p.mat" --WT "~/cwd/data/network/WT.mat" --WP "~/cwd/data/network/KP_priors/WP_noise.mat" --trainWT true --quadquad true | tee infer_1.log
--- data/inference/26/commands.sh
+++ data/inference/26/commands.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 export JULIA_NUM_THREADS=6
 export OPENBLAS_NUM_THREADS=6
-../../../inference.jl "~/cwd/data/perturbation/logFC_outer.mat" 231 199 --J "~/cwd/data/perturbation/J_outer.mat" --epochs 100 --lambda 0.1 --lambdaW 0 --lambdaWT false --WT-prior "~/cwd/data/network/WT_mask.mat" --WP-prior "~/cwd/data/network/KP_mask.mat" --WT-reg "" --WT "~/cwd/data/network/WT.mat" --WP "~/cwd/data/network/KP_priors/WP_noise.mat" --trainWT true --quadquad true | tee infer_1.log
+../../../inference.jl "~/cwd/data/perturbation/logFC_outer.mat" 231 199 --J "~/cwd/data/perturbation/J_outer.mat" --epochs 50 --lambda 0.1 --lambdaW 0 --lambdaWT false --WT-prior "~/cwd/data/network/WT_mask.mat" --WP-prior "~/cwd/data/network/KP_mask.mat" --WT-reg "" --WT "~/cwd/data/network/WT.mat" --WP "~/cwd/data/network/KP_priors/WP_noise.mat" --trainWT true --quadquad true | tee infer_1.log
