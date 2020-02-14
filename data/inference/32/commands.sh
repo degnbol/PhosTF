fd1d1006a17867acd1507641ee757890c8b0a740
Christian <s134801@student.dtu.dk>
2020-02-14 13:14:40 +0100

    reduce from 6 to 4 cores per job

--- data/inference/32/commands.sh
+++ data/inference/32/commands.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
-export JULIA_NUM_THREADS=6
-export OPENBLAS_NUM_THREADS=6
+export JULIA_NUM_THREADS=4
+export OPENBLAS_NUM_THREADS=4
 ../../../inference.jl "~/cwd/data/perturbation/logFC_outer.mat" 231 199 --J "~/cwd/data/perturbation/J_outer.mat" --epochs 50 --lambda 0.1 --lambdaW 0 --lambdaWT true --WT-prior "~/cwd/data/network/WT_mask.mat" --WP-prior "" --WT-reg "~/cwd/data/network/WT_p.mat" --WT "~/cwd/data/network/WT.mat" --WP "~/cwd/data/network/KP_priors/WP_noise.mat" --trainWT true --quadquad true | tee infer_1.log
