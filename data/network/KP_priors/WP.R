119ce8d0ae2d1db98f6f4aaba125987a7e1c180c
Christian <s134801@student.dtu.dk>
2020-02-17 16:53:28 +0100

    adding a WP prior where only KP2TF edges are found from wilcoxon and the rest are noise

--- data/network/KP_priors/WP.R
+++ data/network/KP_priors/WP.R
@@ -8,28 +8,39 @@ flatten = function(x) as.vector(as.matrix(x))
 
 setwd("~/cwd/data/network/KP_priors")
 # read
-KP2TF = read.table("KP2TF_p.tsv", sep="\t", quote="", header=T)
-KP2KP = read.table("KP2KP_p.tsv", sep="\t", quote="", header=T)
-KP_edges = data.frame(KP    =c(KP2TF$KP,     KP2KP$KP),
-                      Target=c(KP2TF$TF,     KP2KP$Target),
-                      weight=c(KP2TF$weight, KP2KP$weight))
-
-# adjacency matrix
 KPs = flatten(read.table("../KP.txt"))
 TFs = flatten(read.table("../TF.txt"))
 KPTFs = c(KPs, TFs)
+KP2TF = read.table("KP2TF_p.tsv", sep="\t", quote="", header=T)
+KP2KP = read.table("KP2KP_p.tsv", sep="\t", quote="", header=T)
+colnames(KP2TF)[colnames(KP2TF)=="TF"] = "Target"
+KP2KP = KP2KP[,colnames(KP2KP)!="p_adj"]
+KP_edges = rbind(KP2TF, KP2KP)
+
+# adjacency matrix
 adjacency = as.matrix(sparseMatrix(i=match(KP_edges$Target, KPTFs), j=match(KP_edges$KP, KPs), x=KP_edges$weight, 
+                                   dims=list(length(KPTFs), length(KPs)), dimnames=list(KPTFs, KPs)))
+adjacency_KP2TF = as.matrix(sparseMatrix(i=match(KP2TF$Target, KPTFs), j=match(KP2TF$KP, KPs), x=KP2TF$weight, 
                                    dims=list(length(KPTFs), length(KPs)), dimnames=list(KPTFs, KPs)))
 write.table(adjacency, "WP.csv", sep=",", quote=F)
 write.table(adjacency, "WP.mat", sep=" ", quote=F, col.names=F, row.names=F)
+write.table(adjacency_KP2TF, "WP_KP2TF.csv", sep=",", quote=F)
+write.table(adjacency_KP2TF, "WP_KP2TF.mat", sep=" ", quote=F, col.names=F, row.names=F)
 
-adjacency_noise = adjacency
-lacking = is.na(adjacency_noise) | (adjacency_noise==0)
-noise_sd = 1/sqrt(sum(dim(adjacency)^2))
-adjacency_noise[lacking] = matrix(rnorm(prod(dim(adjacency)), sd=noise_sd), nrow=nrow(adjacency), ncol=ncol(adjacency))[lacking]
+get_adjacency_noise = function(adjacency) {
+    adjacency_noise = adjacency
+    lacking = is.na(adjacency_noise) | (adjacency_noise==0)
+    noise_sd = 1/sqrt(sum(dim(adjacency)^2))
+    adjacency_noise[lacking] = matrix(rnorm(prod(dim(adjacency)), sd=noise_sd), nrow=nrow(adjacency), ncol=ncol(adjacency))[lacking]
+    adjacency_noise
+}
 
+adjacency_noise = get_adjacency_noise(adjacency)
+adjacency_noise_KP2TF = get_adjacency_noise(adjacency_KP2TF)
 write.table(adjacency_noise, "WP_noise.csv", sep=",", quote=F)
 write.table(adjacency_noise, "WP_noise.mat", sep=" ", quote=F, col.names=F, row.names=F)
+write.table(adjacency_noise_KP2TF, "WP_noise_KP2TF.csv", sep=",", quote=F)
+write.table(adjacency_noise_KP2TF, "WP_noise_KP2TF.mat", sep=" ", quote=F, col.names=F, row.names=F)
 
 
 # analysis
