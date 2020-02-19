035264d963ab6c128579d530aa5414e8ef74d247
Christian <s134801@student.dtu.dk>
2020-02-19 12:32:51 +0100

    save the median diff estimate

--- data/network/KP_priors/KP2TF_wilcoxon.R
+++ data/network/KP_priors/KP2TF_wilcoxon.R
@@ -12,7 +12,7 @@ DT = data.table(KP_pert[KP == kp], TF_regulons)
 # invert Regulon bool since case should be tested for being greater than control, and case is 0 while control is 1 apparently
 # it's very clear which way to test, the values reveal nothing significant if chosen in the wrong direction.
 DT = DT %>% group_by(TF) %>% summarise(p = wilcox.test(abs(M) ~ !Regulon, alternative="g")$p.value, 
-                                       sign = sign(wilcox.test(M ~ !Regulon, conf.int = TRUE)$estimate))
+                                       estimate = wilcox.test(M ~ !Regulon, conf.int=TRUE)$estimate)
 
 DT$KP = kp
 write.table(DT, paste0("KP2TF_parts/", kp, ".tsv"), sep="\t", quote=F, row.names=F)
