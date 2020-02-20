fd6358294701686c4d7a0a7f791f2ee73fd800d6
Christian <s134801@student.dtu.dk>
2020-02-20 12:47:27 +0100

    not in debug mode

--- data/network/KP_priors/KP2TF_wilcoxon.R
+++ data/network/KP_priors/KP2TF_wilcoxon.R
@@ -14,8 +14,8 @@ for(kp in kps) {
     DT = data.table(KP_pert[KP == kp], TF_regulons)
     # invert Regulon bool since case should be tested for being greater than control, and case is 0 while control is 1 apparently
     # it's very clear which way to test, the values reveal nothing significant if chosen in the wrong direction.
-    DT = DT[1:10000, c(wilcox.test(abs(M) ~ !Regulon, alternative="g", conf.int=TRUE)[c("p.value", "estimate")], 
-                        .(sign=sign(wilcox.test(M ~ !Regulon, conf.int=TRUE)$estimate))), by=TF]
+    DT = DT[, c(wilcox.test(abs(M) ~ !Regulon, alternative="g", conf.int=TRUE)[c("p.value", "estimate")], 
+                .(sign=sign(wilcox.test(M ~ !Regulon, conf.int=TRUE)$estimate))), by=TF]
     
     
     DT$KP = kp
