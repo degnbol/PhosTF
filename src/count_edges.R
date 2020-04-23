e76f61ecc3d6ff9e8c1a064987e230121f98af39
Christian <s134801@student.dtu.dk>
2020-04-23 16:51:07 +0200

    show both TF->TF and TF->O

--- src/count_edges.R
+++ src/count_edges.R
@@ -3,20 +3,27 @@
 library(data.table)
 
 #######################################################
-# count edges KP->KP, KP->TF, TF->KP, TF->TF
+# count edges KP->KP, KP->TF, TF->KP, TF->TF, TF->O
 #######################################################
 
+# first half is WPs second half is WTs
 args = commandArgs(T)
-WP = as.matrix(fread(args[1]))
-WT = as.matrix(fread(args[2]))
+stopifnot(length(args) %% 2 == 0)
+n = length(args) / 2
 
-nKP = ncol(WP)
-
-nKP2KP = sum(WP[1:nKP,] != 0)
-nKP2TF = sum(WP[nKP+1:nrow(WP),] != 0)
-nTF2KP = sum(WT[1:nKP,] != 0)
-nTF2TF = sum(WT[nKP+1:nrow(WT),] != 0)
-
-cat(paste(c(nKP2KP,nKP2TF,nTF2KP,nTF2TF), sep="\n"), "\n")
-
+for (i in 1:n) {
+    WP = as.matrix(fread(args[i]))
+    WT = as.matrix(fread(args[i+n]))
+    
+    nKP = ncol(WP)
+    nTF = ncol(WT)
+    
+    nKP2KP = sum(WP[1:nKP,] != 0)
+    nKP2TF = sum(WP[(nKP+1):nrow(WP),] != 0)
+    nTF2KP = sum(WT[1:nKP,] != 0)
+    nTF2TF = sum(WT[nKP+(1:nTF),] != 0)
+    nTF2O  = sum(WT[(nKP+nTF+1):nrow(WT),] != 0)
+    
+    cat(paste(c(nKP2KP,nKP2TF,nTF2KP,nTF2TF,nTF2O)), "\n")
+}
 
