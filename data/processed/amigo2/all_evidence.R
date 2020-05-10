945aa18601011ebd82461266dea2030ea3144d4e
Christian <s134801@student.dtu.dk>
2020-05-10 22:11:55 +0200

    collect all evidence for each ORF

---
+++ data/processed/amigo2/all_evidence.R
@@ -0,0 +1,59 @@
+#!/usr/bin/env Rscript
+# list all evidence together
+
+library(data.table)
+
+# functions
+unwhich = function(which, dim=max(which)) {
+    y = array(logical(dim), dim=dim)
+    y[which] = TRUE
+    y
+}
+
+read_go = function(path, label) {
+    go = fread(path, col.names=c("Evidence", "ORF"))
+    go[,Label:=label]
+    return(go)
+}
+
+setwd("~/cwd/data/processed/amigo2")
+
+# read GO
+
+GO = rbind(
+    read_go("activators.tsv", "TF_activator"),
+    read_go("repressors.tsv", "TF_repressor"),
+    read_go("protein_kinase.tsv", "PK"),
+    read_go("protein_phosphatase.tsv", "PP"),
+    read_go("protein_phosphorylation.tsv", "PK"),
+    read_go("protein_dephosphorylation.tsv", "PP")
+)
+
+# split into 3 categories of evidence
+# http://wiki.geneontology.org/index.php/Guide_to_GO_Evidence_Codes
+experimental_evidence = c("HMP", "IMP", "HDA", "IGI", "IDA", "IPI")
+computational_evidence = c("IEA", "IGC", "RCA", "IBA", "ISO", "ISA", "ISS", "ISM")
+statement_evidence = c("IC", "NAS", "TAS")
+nonexperimental_evidence = c(computational_evidence, statement_evidence)
+
+GO[Evidence%in%experimental_evidence,Experimental:=Evidence]
+# GO[Evidence%in%nonexperimental_evidence,Nonexperimental:=Evidence]
+GO[,Evidence:=NULL]
+for(label in unique(GO$Label)) {
+    GO[Label==label,paste(label,"Experimental",sep="_"):=Experimental]
+    # GO[Label==label,paste(label,"Nonexperimental",sep="_"):=Nonexperimental]
+}
+GO[,Experimental:=NULL]
+# GO[,Nonexperimental:=NULL]
+GO[,Label:=NULL]
+GO[is.na(GO)] = ""
+
+GO = GO[, lapply(.SD, function(x) paste(x[x!=""], collapse=",")), by=ORF]
+
+
+
+
+
+
+
+
