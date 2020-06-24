9504d884eaca92ae882a3dcffcb361c4bcb67959
degnbol <christian.degnbol@gmail.com>
2020-06-24 16:56:14 +0200

    separate julia and shell commands

--- data/dream/simulations_KP/commands.sh
+++ data/dream/simulations_KP/commands.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env zsh
-#mkdir {1..5}_{1..5}
+mkdir {1..5}_{1..5}
 for mat in {1..5}; do for sample in {1..5}; do
-    #ln -sf ../../goldstandard/goldstandard_$mat.mat ${mat}_${sample}/goldstandard.mat
-    cd ${mat}_${sample}
-    # make WT and WP with {-1, 0, 1} for deactvation, no edge and activation
-    #~/cwd/weight.jl random goldstandard.mat 30
-    # make net.bson with a fully defined network instance with all it's constants, etc.
-    ~/cwd/simulation.jl network
-    cd -
+    ln -sf ../../goldstandard/goldstandard_$mat.mat ${mat}_${sample}/goldstandard.mat
 done; done
