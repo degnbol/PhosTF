7d2e98497c4918b8b0029b1eecb407e09cef2f00
Christian <s134801@student.dtu.dk>
2020-06-25 18:05:36 +0200

    sanity check for all the neg neg regulations

---
+++ data/inference/74/regreg_sanity_check.R
@@ -0,0 +1,28 @@
+#!/usr/bin/env Rscript
+
+library(data.table)
+
+
+KP_edges = fread("~/cwd/data/inference/74/KP_edges_eval_noTN_protein.tsv", sep="\t", header=T)
+perturbation = fread("~/cwd/data/perturbation/logFC_inner.csv", sep=",", header=T)
+# make sure we are reading a version where first column is named ORF (data.table format) and not without row names header (data.frame format)
+for(n in colnames(perturbation)) {stopifnot(is.character(n))}
+
+perturb_names = colnames(perturbation)[2:ncol(perturbation)]
+
+KP_edges = KP_edges[(target_type=="TF") & (inferred == T) & (KP%in%perturb_names) & (Target%in%perturb_names),]
+
+pert_prods = function(edges) {
+    out = c()
+    for (i in 1:nrow(edges)) {
+        out = c(out, perturbation[,edges$KP[i],with=F] * perturbation[, edges$Target[i], with=F])
+    }
+    return(unlist(out))
+}
+
+prods = pert_prods(KP_edges)
+
+
+
+
+
