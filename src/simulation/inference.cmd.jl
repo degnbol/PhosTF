7f581f123c75974209268aef290634941b93e44c
degnbol <christian.degnbol@gmail.com>
2020-06-27 19:13:25 +0200

    redirect stdout to logfile

--- src/simulation/inference.cmd.jl
+++ src/simulation/inference.cmd.jl
@@ -10,8 +10,14 @@ for dir in ARGS
 
     nᵥ, nₜ = size(loaddlm("WT.mat"))
     _,  nₚ = size(loaddlm("WP.mat"))
-    infer("X_sim.mat", nₜ, nₚ; epochs=15000, lambda=.1)
-        
+
+    open("infer.log", "w") do log redirect_stdout(log) do
+            infer("X_sim.mat", nₜ, nₚ; epochs=15000, lambda=.1)
+    end end
+    
+    rm("WT.tmp.mat")
+    rm("WP.tmp.mat")
+
     cd("..")
 end
 
