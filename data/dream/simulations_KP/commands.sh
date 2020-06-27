7bd32a6b6fccf9a2879ebbd8153e120f84042119
degnbol <christian.degnbol@gmail.com>
2020-06-27 18:25:07 +0200

    add julia commands to the general commands file for history

--- data/dream/simulations_KP/commands.sh
+++ data/dream/simulations_KP/commands.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env zsh
+
+# make folders and link goldstandard
 mkdir {1..5}_{1..5}
 for mat in {1..5}; do for sample in {1..5}; do
     ln -sf ../../goldstandard/goldstandard_$mat.mat ${mat}_${sample}/goldstandard.mat
 done; done
+
+# simulate
+~/cwd/src/simulation/simulate.cmd.jl ?_?/
+
+# infer
+~/cwd/src/simulation/inference.cmd.jl ?_?/
+
