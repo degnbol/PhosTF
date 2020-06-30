d8b06f71186e3c2514063a7ec81a9c65c636bccb
degnbol <christian.degnbol@gmail.com>
2020-06-30 13:40:10 +0200

    remove unused quadquad and PKPP

--- data/inference/08/commands_PKPP.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env zsh
-export JULIA_NUM_THREADS=4
-export OPENBLAS_NUM_THREADS=4
-../../../inference.jl "~/cwd/data/perturbation/logFC_inner.mat" 231 199 --J "~/cwd/data/perturbation/J_inner.mat" --epochs 10 --lambda 0.1 --lambdaW 0 --lambdaWT true --WT-prior "~/cwd/data/network/WT_mask.mat" --WP-prior "" --WT-reg "~/cwd/data/network/WT_p.mat" --WT "WT_infer.mat" --WP "WP_infer.mat" --trainWT true --quadquad false --PKPP "~/cwd/data/network/PKPP.txt" | tee infer_PKPP.log
--- data/inference/38/commands_PKPP.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env zsh
-export JULIA_NUM_THREADS=4
-export OPENBLAS_NUM_THREADS=4
-../../../inference.jl "~/cwd/data/perturbation/logFC_inner.mat" 231 199 --J "~/cwd/data/perturbation/J_inner.mat" --epochs 10 --lambda 0 --lambdaW 0.1 --lambdaWT false --WT-prior "" --WP-prior "~/cwd/data/network/KP_mask.mat" --WT-reg "" --WT "~/cwd/data/network/WT.mat" --WP "WP_infer.mat" --trainWT false --quadquad false --PKPP "~/cwd/data/network/PKPP.txt" | tee infer_PKPP.log
