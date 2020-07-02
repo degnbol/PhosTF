75f333629371c5360c9c6052118375c16eb907dc
Christian <s134801@student.dtu.dk>
2020-07-02 13:37:31 +0200

    protein KP

---
+++ data/network/proteinKP.R
@@ -0,0 +1,25 @@
+#!/usr/bin/env Rscript
+
+library(data.table)
+
+
+setwd("~/cwd/data/network")
+
+
+TFs = fread("TF_mode.tsv", sep="\t")
+KPs = fread("KP.tsv", sep="\t")
+biological_DT = fread("node_attributes_full.txt", sep="\t")
+
+# TF set is not changed
+stopifnot(length(intersect(biological_DT[protein_type0 == "TF",.(ORF)]$ORF, TFs$TF)) == nrow(TFs))
+stopifnot(all(TFs[TF%in%biological_DT[protein_type2=="TFA",.(ORF)]$ORF,.(Mode)]$Mode == "activator"))
+stopifnot(all(TFs[TF%in%biological_DT[protein_type2=="TFR",.(ORF)]$ORF,.(Mode)]$Mode == "repressor"))
+
+# more KPs has been classified by Chris
+KPs[KP%in%biological_DT[protein_type == "PK",.(ORF)]$ORF]$Mode
+KPs[KP%in%biological_DT[protein_type == "PP",.(ORF)]$ORF]$Mode
+
+proteinKP = biological_DT[protein_type0=="KP",.(ORF, protein_type)]
+fwrite(proteinKP, "KP_protein.tsv", sep="\t")
+
+
