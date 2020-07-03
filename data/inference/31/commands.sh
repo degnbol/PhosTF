b3ed03d1f1ee807ddcbea9fae6b530c6f77c54a5
degnbol <christian.degnbol@gmail.com>
2020-07-03 21:44:39 +0200

    setting up for protein KP infer

--- data/inference/31/commands.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env zsh
-export JULIA_NUM_THREADS=4
-export OPENBLAS_NUM_THREADS=4
-../../../inference.jl "~/cwd/data/perturbation/logFC_outer.mat" 231 199 --J "~/cwd/data/perturbation/J_outer.mat" --epochs 50 --lambda 0.1 --lambdaW 0 --lambdaWT true --WT-prior "" --WP-prior "" --WT-reg "~/cwd/data/network/WT_p.mat" --WT "~/cwd/data/network/WT.mat" --WP "~/cwd/data/network/KP_priors/WP_noise.mat" --trainWT true | tee infer_1.log
--- data/inference/37/commands.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env zsh
-export JULIA_NUM_THREADS=4
-export OPENBLAS_NUM_THREADS=4
-../../../inference.jl "~/cwd/data/perturbation/logFC_inner.mat" 231 199 --J "~/cwd/data/perturbation/J_inner.mat" --epochs 50 --lambda 1 --lambdaW 1 --lambdaWT false --WT-prior "" --WP-prior "~/cwd/data/network/KP_mask.mat" --WT-reg "" --WT "~/cwd/data/network/WT.mat" --WP "~/cwd/data/network/KP_priors/WP_noise.mat" --trainWT false | tee infer_1.log
