333bec3e166415ae583ee51bda03e4c7721a52aa
Christian <s134801@student.dtu.dk>
2020-07-15 14:55:10 +0200

    simulate from steady state wildtype when simulating a knockout

--- data/toy/simulation/simulation.cmd.jl
+++ data/toy/simulation/simulation.cmd.jl
@@ -4,6 +4,9 @@ expanduser("~/cwd/simulation.jl") |> include
 
 network()
 simulate()
+for mut_id in 1:4
+    simulate(mut_id; r0="sim_r.mat", p0="sim_p.mat", psi0="sim_psi.mat")
+end
 plot(3, 2; o="sim.pdf")
 steadystate()
 logFC()
--- src/plot_simulation.R
+++ src/plot_simulation.R
@@ -5,16 +5,11 @@ library(reshape2)
 library(grid)
 library(gridExtra)
 
-# dev packages for flipping x axis of mutant
-library(scales)
-library(facetscales)
 
 # constants ####
 mRNA = "mRNA"
 prot = "protein"
 phos = "active protein"
-wildtype = "Wildtype"
-mutant = "KP1 Knockout"
 
 KP_colors = c("#af70b6", "#654169", "#9f3b5a", "#9400d3")
 TF_colors = c("#87ac33", "#50661e", "#2fa540", "#8FBC8F")
@@ -87,12 +82,20 @@ readfunc = function(fnames) {
 # setwd("~/cwd/data/testdata/pres18c"); nt = 3; np = 3; no = 0
 # setwd("~/cwd/data/testdata/simi"); nt = 2; np = 3; no = 1
 # setwd("~/cwd/data/toy/simulation"); nt = 2; np = 3; no = 1
-args = commandArgs(T)
+args = as.numeric(commandArgs(T))
 np = args[1]
 nt = args[2]
 no = args[3]
-mut = args[4]
-outfname = paste0("simulation_", mut, ".pdf")
+mutnum = args[4]
+outfname = paste0("simulation_", mutnum, ".pdf")
+
+# facet titles
+wildtype = "Wildtype"
+if(mutnum <= np) {
+    mutant = paste0("KP", mutnum, " knockout") 
+} else if(mutnum <= np+nt) {
+    mutant = paste0("TF", mutnum, " knockout")
+}
 
 n = np+nt+no
 wt_fnames = list(
@@ -102,10 +105,10 @@ wt_fnames = list(
     t = "sim_t.mat"
 )
 mut_fnames = list(
-    r = paste0("sim_r_", mut, ".mat"),
-    p = paste0("sim_p_", mut, ".mat"),
-    psi = paste0("sim_psi_", mut, ".mat"),
-    t = paste0("sim_t_", mut, ".mat")
+    r = paste0("sim_r_", mutnum, ".mat"),
+    p = paste0("sim_p_", mutnum, ".mat"),
+    psi = paste0("sim_psi_", mutnum, ".mat"),
+    t = paste0("sim_t_", mutnum, ".mat")
 )
 # process ####
 
@@ -140,8 +143,11 @@ p = ggplot(data, aes(x=time, y=value, group=variable, color=node)) +
 # without flipping x-axis of mutant
 p = p + facet_grid(vars(type), vars(experiment), scales="free_x")
 # or with flipping x-axis of mutant
-# x_scales = list(Wildtype=scale_x_continuous(), Mutant=scale_x_reverse())
-# p = p + facet_grid_sc(vars(type), vars(experiment), scales=list(x=x_scales))
+# # dev packages for flipping x axis of mutant
+#library(scales)
+#library(facetscales)
+#x_scales = list(Wildtype=scale_x_continuous(), Mutant=scale_x_reverse())
+#p = p + facet_grid_sc(vars(type), vars(experiment), scales=list(x=x_scales))
 
 ggsave(outfname, p, width=7, height=5)
 
