e1ceb9f36713105e2aee45c8d9a62ec153fd52dc
Christian <s134801@student.dtu.dk>
2020-07-21 16:37:28 +0200

    get the KP targets with no known site without first filtering for evaluation set

--- data/network/commands.sh
+++ data/network/commands.sh
@@ -9,6 +9,9 @@ sed 's/-1/-/g' WT_FDR20_sign.mat | sed 's/1/+/g' > WT_FDR20_sign_mask.mat
 sed 's/-//g' WT_FDR20_sign.mat > WT_FDR20_abssign_mask.mat
 
 ./KP_mask.R
+sed 1d KP_mask.csv | cut -f-2 -d, | grep '0$' | sed 's/,0//' > KP_targets_noknownsite.txt
+gzip --keep KP_targets_noknownsite.txt
+
 # now to go KP_priors
 ./KP_priors/commands.sh
 
