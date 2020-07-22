de1b445be14d748160fa3d78ab78473480abf3f3
Christian <s134801@student.dtu.dk>
2020-07-22 18:46:21 +0200

    use on different file

--- data/inference/proteinKP.R
+++ data/inference/proteinKP.R
@@ -1,10 +1,10 @@
-#!/usr/bin/env Rcript
+#!/usr/bin/env Rscript
 
 # filter KP edges for protein KP. same procedure as in ~/cwd/src/square_euler.R
 
 library(data.table)
 
-KP_edges = fread("~/cwd/data/inference/74/KP_edges_eval_noTN.tsv", sep="\t", header=T)
+KP_edges = fread("~/cwd/data/inference/74/KP_edges.tsv", sep="\t", header=T)
 protein_KPTF_table = read.table("~/cwd/data/network/node_attributes_full.txt", sep="\t", header=T)
 protein_KP = as.character(protein_KPTF_table$ORF[protein_KPTF_table$protein_type0=="KP" & !is.na(protein_KPTF_table$protein_type0)])
 protein_TF = as.character(protein_KPTF_table$ORF[protein_KPTF_table$protein_type0=="TF" & !is.na(protein_KPTF_table$protein_type0)])
@@ -14,7 +14,7 @@ protein_KPTF = c(protein_KP, protein_TF)
 KP_edges = KP_edges[as.character(KP_edges$Target) %in% protein_KPTF,]
 KP_edges = KP_edges[as.character(KP_edges$KP) %in% protein_KPTF,]
 
-fwrite(KP_edges, "~/cwd/data/inference/74/KP_edges_eval_noTN_protein.tsv", sep="\t")
+fwrite(KP_edges, "~/cwd/data/inference/74/KP_edges_protein.tsv", sep="\t")
 
 
 
