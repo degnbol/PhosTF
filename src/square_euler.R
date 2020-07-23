c92c256e8e88ab50a5e35edb2d0f16e72af22fb9
Christian <s134801@student.dtu.dk>
2020-07-23 18:37:24 +0200

    code significantly cleaned up. looks more correct. takes KP masking into account.

--- src/square_euler.R
+++ src/square_euler.R
@@ -1,12 +1,11 @@
 #!/usr/bin/env Rscript
 
 # packages
+library(reshape2)
+library(Matrix)
+library(eulerr)
+library(ggplot2)
 library(data.table)
-suppressPackageStartupMessages(library(reshape2))
-suppressPackageStartupMessages(library(Matrix))
-suppressPackageStartupMessages(library(eulerr))
-suppressPackageStartupMessages(library(ggplot2))
-library(latex2exp)
 
 # functions
 flatten = function(x) as.vector(as.matrix(x))
@@ -22,22 +21,72 @@ unwhich = function(which, dim=max(which)) {
     y[which] = TRUE
     y
 }
-# convert tex code to expression object (use \\ for \ and combine expressions with list() instead of c())
-tex = function(x) unname(TeX(paste0("$",x)))
+# evalset is bool vector indicating true and false edges.
+# drawn is a bool vector of same size indicating the edges that are inferred.
+get_p = function(evalset, drawn) {
+    q = sum(evalset[drawn])  # number of correct inferences
+    m = sum(evalset) # number of true edges
+    n = length(evalset) - m  # number of black balls to pick/"false" edges/potential edges not in set of known
+    k = sum(drawn)  # number of inferred edges
+    phyper(q, m, n, k, lower.tail=F)
+}
+# DT has columns substrate, infer, eval. Substrate is "KP" or "TF". infer and eval are Boolean.
+# ps is p-value for KP2KP and KP2TF
+plot_square_euler(DT, ps) {
+    ## constants
+    KP_color = "#bd61b6"
+    TF_color = "#75b42f"
+    bw = .8
+    hn = .05  # horizontal nudge
+    fgscl = 4  # how wide foreground is compared to background
+    # version where we nudge foregrounds down 1/4
+    fgdg = bw/4
+    
+    
+    ## make background and foreground bars
+    bg = DT[       , .N, by=c("substrate", "infer")]
+    fg = DT[eval==T, .N, by=c("substrate", "infer")]
+    # labels
+    bg[,label:=fifelse(infer, "Inferred", "Not inferred")]
+    fg[,label:=substrate]
+    # bar heights
+    bg[, y:=fifelse(infer, -N, N)]
+    fg[, y:=fifelse(infer, -N, N)]
+    # horizontal adjustment
+    bg[, hjust:=fifelse(infer, -hn, 1+hn)]
+    fg[, hjust:=fifelse(infer, 1+hn, -hn)]
+    # x
+    bg[, x:=fifelse(substrate == "KP", 1, 2) + bw/2-.01]
+    fg[, x:=fifelse(substrate == "KP", 1, 2) - fgdg]
+    # colors
+    fg[, color:=fifelse(substrate == "KP", KP_color, TF_color)]
+    
+    ggplot(mapping=aes(x=substrate, y=y)) + 
+        # outline
+        geom_bar(data=bg, mapping=aes(fill=label), stat="identity", position="identity", width=bw, fill="transparent", color="black", size=2) +
+        # gray and white background bars
+        geom_bar(data=bg, mapping=aes(fill=label), stat="identity", position="identity", width=bw) +
+        # colored foreground bars with eval data
+        geom_bar(data=fg, mapping=aes(fill=substrate, y=y*fgscl), stat="identity", position=position_nudge(x=-fgdg), width=bw/fgscl, alpha=.5) +
+        scale_fill_manual(values=list(KP=KP_color, TF=TF_color, Inferred="lightgray", `Not inferred`="white")) +
+        # labels saying "Inferred" and "Not inferred"
+        annotate("text", x=bg$x, vjust=1,   y=bg$y, hjust=bg$hjust, label=bg$label, fontface="italic", alpha=.7) +
+        # numbers under those labels
+        annotate("text", x=bg$x, vjust=2.2, y=bg$y, hjust=bg$hjust, label=bg$N,     fontface="bold", alpha=.8) +
+        # labels "Known"
+        annotate("text", x=c(1,2)-fgdg+bw/fgscl, vjust=0, hjust=.5, y=0, label="Known", fontface="italic", color=c(KP_color, TF_color)) +
+        # labels with counts
+        annotate("text", x=fg$x, vjust=0.5, y=fg$y*fgscl, hjust=fg$hjust, label=fg$N, fontface="bold", color=fg$color) +
+        # p-values
+        annotate("text", x=c(1,2)-fgdg, vjust=0.5, hjust=0, y=6000, label=sprintf("p=%.3g",ps), color=c(KP_color, TF_color)) +
+        coord_flip() + theme_void() + 
+        theme(legend.position="none", axis.text.y=element_text(), plot.margin=margin(t=0,b=0,l=0,r=0,unit="pt")) +
+        scale_x_discrete(labels=c("d(KP,KP)", "d(KP,TF)"))
+}
 
 
-#KP_edge_fname = "~/cwd/data/inference/74/KP_edges.tsv"
-KP_edge_fnames = commandArgs(trailingOnly=T)
-P_fname = "~/cwd/data/evaluation/P_eval.tsv"
-KP_targets_noknownsite = read.vector("~/cwd/data/network/KP_targets_noknownsite.txt")
-KP = fread("~/cwd/data/network/KP_protein.tsv")$ORF
-TF = read.vector("~/cwd/data/network/TF.txt")
-V = read.vector("~/cwd/data/network/V_protein.txt")
-KPTF = c(KP,TF)
-nKP = length(KP)
-nV = length(V)
-nTF = length(TF)
-nO = nV-length(KPTF)
+
+
 
 
 ## settings
@@ -45,119 +94,83 @@ nO = nV-length(KPTF)
 masking_KP = TRUE
 
 
+# read evaluation files
+KP = fread("~/cwd/data/network/KP_protein.tsv")$ORF
+TF = read.vector("~/cwd/data/network/TF.txt")
+V  = read.vector("~/cwd/data/network/V_protein.txt")
+KPTF = c(KP,TF)
+nKP = length(KP)
+nV = length(V)
+nTF = length(TF)
+nO = nV-length(KPTF)
+KP_eval = fread("~/cwd/data/evaluation/P_eval.tsv", 
+                select=c("Source", "Target", "biogrid", "fasolo", "parca", "fiedler", "yeastkid", "ptmod", "ptacek"))
+# remove diagonals
+KP_eval = KP_eval[Source!=Target,]
+# make evaluations Boolean
+KP_eval[,biogrid:=biogrid!=""]
+KP_eval[,fiedler:=fiedler!=""]
+KP_eval[,fasolo:=!is.na(fasolo)]
+KP_eval[,parca:=!is.na(parca)]
+KP_eval[,ptacek:=!is.na(ptacek)]
+# 4.52 corresponds to p<0.05 according to yeastKID
+KP_eval[,yeastkid:=!is.na(yeastkid) & yeastkid>4.52]
+# string DB scores are confidence from 0 to 1 written as e.g. 800 to mean 0.8.
+# higher confidence score is better. 0.5 means that there is 50% prob for false positive. https://string-db.org/cgi/info.pl
+# if we use False Discovery Rate = .2 it only gives us a meager 4 edges, all of which are already in other datasets.
+# # so we use a dangerous FDR = .4, which gives us 7 edges not already found in another evaluation set.
+KP_eval[,ptmod:=!is.na(ptmod) & ptmod>600]
+KP_eval[,literature:=biogrid|fasolo|parca|fiedler]
+KP_eval[,curated:=yeastkid|ptmod]
+KP_eval[,known:=literature|curated]
+KP_eval[,invitro:=known|ptacek]
+# not evaluating on uninferrable edges
+KP_eval$possible = T
+if(masking_KP) {
+    KP_targets_noknownsite = read.vector("~/cwd/data/network/KP_targets_noknownsite.txt")
+    KP_eval[Target%in%KP_targets_noknownsite,possible:=F]
+}
+
+
+
+#KP_edge_fname = "~/cwd/data/inference/03/KP_edges.tsv"
+KP_edge_fnames = commandArgs(trailingOnly=T)
 rundir = getwd()
 
 for (KP_edge_fname in KP_edge_fnames) {
     cat(KP_edge_fname, "\n")
     setwd(dirname(KP_edge_fname))
-    KP_edge_fname = basename(KP_edge_fname)
-    KP_edges = read.table(KP_edge_fname, header=T, sep="\t", quote="", check.names=F)
-    P_eval = read.table(P_fname, header=T, sep="\t", quote="", check.names=F)
-    # remove diagonals
-    P_eval = P_eval[as.character(P_eval$Source) != as.character(P_eval$Target),]
-    # filter for PROTEIN kinase, phosphatases and transcription factors
-    KP_edges = KP_edges[as.character(KP_edges$Target) %in% KPTF,]
-    P_eval = P_eval[as.character(P_eval$Target) %in% KPTF,]
-    KP_edges = KP_edges[as.character(KP_edges$KP) %in% KPTF,]
-    P_eval = P_eval[as.character(P_eval$Source) %in% KPTF,]
+    KP_edges = fread(basename(KP_edge_fname), drop="marker")
+    KP_edges$infer = KP_edges$q < .05
+    KP_edges[,q:=NULL]
+    
+    # all targets should be protein KP or TF
+    stopifnot(all(KP_edges$KP%in%KP))
+    stopifnot(all(KP_edges$Target%in%KPTF))
+    stopifnot(all(KP_eval$Source%in%KP))
+    stopifnot(all(KP_eval$Target%in%KPTF))
     # sanity check
-    stopifnot(all(as.character(KP_edges$KP) == as.character(P_eval$Source)))
-    stopifnot(all(KP_edges$Target == P_eval$Target))
-    # hold all data in eval sets
-    P_eval$marker = KP_edges$marker
-    top_marked = KP_edges$q < .05
+    stopifnot(all(KP_edges$KP == KP_eval$Source))
+    stopifnot(all(KP_edges$Target == KP_eval$Target))
+    # add eval data
+    KP_edges = cbind(KP_edges, KP_eval[,!c("Source", "Target")])
+    # filter for possible edges and remove then redundant column
+    KP_edges = KP_edges[possible==T,][,possible:=NULL]
     
-    # only analysing positives, since we cannot actually rule out negatives
+    KP_edges[Target%in%KP,Target:="KP"]
+    KP_edges[Target%in%TF,Target:="TF"]
+    KP_edges[,KP:=NULL]
     
-    venndata = data.frame(
-        potential = TRUE,
-        biogrid = P_eval$biogrid != "",
-        fasolo = !is.na(P_eval$fasolo),
-        parca = !is.na(P_eval$parca),
-        fiedler = P_eval$fiedler != "",
-        yeastkid = !is.na(P_eval$yeastkid) & P_eval$yeastkid > 4.52,
-        ptmod = !is.na(P_eval$ptmod) & P_eval$ptmod > 250,
-        ptacek = !is.na(P_eval$ptacek)
-    )
-    venndata$literature = venndata$biogrid | venndata$fasolo | venndata$parca | venndata$fiedler
-    venndata$curated = venndata$yeastkid | venndata$ptmod
-    venndata$known = venndata$literature | venndata$curated
-    venndata$invitro = venndata$known | venndata$ptacek
-
-    # not evaluating on uninferrable edges
-    if(masking_KP) {
-        venndata[P_eval$Target%in%KP_targets_noknownsite,] = FALSE
-    }
-    
-    p.selection = function(col_select, row_idx) {
-        drawn = top_marked[row_idx]
-        if(missing(row_idx)) row_idx = rep(TRUE, nrow(venndata))
-        evaldata = venndata[row_idx, col_select]
-        q = sum(evaldata[drawn])  # number of correct inferences
-        m = sum(evaldata) # number of true edges
-        n = sum(venndata[row_idx, "potential"]) - m  # number of black balls to pick/"false" edges/potential edges not in set of known
-        list(value=q, p=phyper(q, m, n, sum(drawn), lower.tail=FALSE))
-    }
-    
-    KP2KP.idx = P_eval$Target%in%KP
-    KP2TF.idx = P_eval$Target%in%TF
-    stopifnot(all(KP2KP.idx|KP2TF.idx))
-    
-    ### Plotting
-    KP_color = "#bd61b6"
-    TF_color = "#75b42f"
-    
-    plot_square_euler = function(selection) {
-        eulerdata.KP = data.frame(venndata[KP2KP.idx,c("potential", selection)], inferred=top_marked[KP2KP.idx])
-        eulerdata.TF = data.frame(venndata[KP2TF.idx,c("potential", selection)], inferred=top_marked[KP2TF.idx])
+    for(evalname in c("known", "literature", "invitro")) {
         
-        euler.plt.bg = rbind(data.frame(substrate="KP", inferred =-sum(eulerdata.KP$potential &  eulerdata.KP$inferred), 
-                                                        potential= sum(eulerdata.KP$potential)),
-                             data.frame(substrate="TF", inferred =-sum(eulerdata.TF$potential &  eulerdata.TF$inferred),
-                                                        potential= sum(eulerdata.TF$potential)))
-        euler.plt.fg = rbind(data.frame(substrate="KP", TP       =-sum(eulerdata.KP$potential &  eulerdata.KP$inferred & eulerdata.KP[,selection]),
-                                                        FN       = sum(eulerdata.KP$potential & !eulerdata.KP$inferred & eulerdata.KP[,selection])),
-                             data.frame(substrate="TF", TP       =-sum(eulerdata.TF$potential &  eulerdata.TF$inferred & eulerdata.TF[,selection]),
-                                                        FN       = sum(eulerdata.TF$potential & !eulerdata.TF$inferred & eulerdata.TF[,selection])))
-        euler.plt.bg = melt(euler.plt.bg, id.vars="substrate")
-        euler.plt.fg = melt(euler.plt.fg, id.vars="substrate")
-        euler.plt.bg$count = abs(euler.plt.bg$value)
-        # reduce the size of the area for "not inferred" region to exclude "inferred" since the label is "potential".
-        euler.plt.bg$value[euler.plt.bg$variable == "potential" & euler.plt.bg$substrate == "KP"] = sum(euler.plt.bg$count[euler.plt.bg$substrate == "KP"])
-        euler.plt.bg$value[euler.plt.bg$variable == "potential" & euler.plt.bg$substrate == "TF"] = sum(euler.plt.bg$count[euler.plt.bg$substrate == "TF"])
+        DT = KP_edges[, c("Target", "infer", evalname), with=F]
+        ps = c(get_p(DT[Target=="KP"][[evalname]], DT[Target=="KP"][["infer"]]), 
+               get_p(DT[Target=="TF"][[evalname]], DT[Target=="TF"][["infer"]]))
         
-        
-        pvalues = c(p.selection(selection, KP2KP.idx)$p,
-                    p.selection(selection, KP2TF.idx)$p)
-        
-        
-        bw = .8
-        hn = .05  # horizontal nudge
-        fgscl = 4  # how wide foreground is compared to background
-        
-        # version where we nudge fgs down 1/4
-        fgdg = bw/4
-        
-        ggplot(mapping=aes(x=substrate, y=value)) + 
-            geom_bar(data=euler.plt.bg, mapping=aes(fill=variable), stat="identity", position="identity", width=bw, fill="transparent", color="black", size=2) +
-            geom_bar(data=euler.plt.bg, mapping=aes(fill=variable), stat="identity", position="identity", width=bw) +
-            geom_bar(data=euler.plt.fg, mapping=aes(fill=substrate, y=value*fgscl), stat="identity", position=position_nudge(x=-fgdg), width=bw/fgscl, alpha=.5) +
-            scale_fill_manual(values=list(KP=KP_color, TF=TF_color, inferred="lightgray", potential="white")) +
-            annotate("text", x=c(1,2,1,2)+bw/2-.01, vjust=1,   y=euler.plt.bg$value, hjust=c(-hn,-hn,1+hn,1+hn), label=euler.plt.bg$variable, fontface="italic", alpha=c(.65,.65,1,1)) +
-            annotate("text", x=c(1,2,1,2)+bw/2-.01, vjust=2.2, y=euler.plt.bg$value, hjust=c(-hn,-hn,1+hn,1+hn), label=euler.plt.bg$count, fontface="bold", alpha=c(.65,.65,1,1)) +
-            annotate("text", x=c(1,2)-fgdg+bw/fgscl, vjust=0, hjust=.5, y=0, label="known", fontface="italic", color=c(KP_color, TF_color)) +
-            annotate("text", x=c(1,2,1,2)-fgdg, vjust=0.5, y=euler.plt.fg$value*fgscl, hjust=c(1+hn,1+hn,-hn,-hn), label=abs(euler.plt.fg$value), fontface="bold", color=rep(c(KP_color, TF_color),2)) +
-            annotate("text", x=c(1,2)-fgdg, vjust=0.5, hjust=0, y=6000, label=sprintf("p=%.3g",pvalues), color=c(KP_color, TF_color)) +
-            coord_flip() + theme_void() + 
-            theme(legend.position="none", axis.text.y=element_text(),
-                  plot.margin=margin(t=0,b=0,l=0,r=0,unit="pt")) +
-            scale_x_discrete(labels=parse(text=c(tex("KP\\rightarrow KP"),tex("KP\\rightarrow TF"))))
-        
-    }
-    
-    selections = c("known","literature","invitro")
-    for(selection in selections) {
-        ggsave(paste0("square_euler_", selection, ".pdf"), plot=plot_square_euler(selection), width=6.5, height=1.35)
+        setnames(DT, "Target", "substrate")
+        setnames(DT, evalname, "eval")
+        plt = plot_square_euler(DT, ps)
+        ggsave(paste0("square_euler_", evalname, ".pdf"), plot=plt, width=6.5, height=1.35)
     }
     
     setwd(rundir)  # go back to so relative dirs for other files still work
