c04a5a5f8cd3e4b1fb0906415e83e48316ddb9e6
degnbol <cdegma@dtu.dk>
2020-08-04 12:25:21 +0200

    allow for non training of WT

--- data/inference/continue.sh
+++ data/inference/continue.sh
@@ -12,7 +12,10 @@ for r in $@; do
     mv infer{,_1}.log
     mv commands{,_1}.sh
     let epochs=100-$(tail -n1 infer_1.log | grep -o $'\t[0-9][0-9]\t' | xargs)
-    sed "s/--epochs 100/--epochs $epochs/" commands_1.sh | sed -E 's/--WT "\S+"/--WT "WT.tmp.mat"/' | sed -E 's/--WP "\S+"/--WP "WP.tmp.mat"/' > commands_2.sh
+    sed "s/--epochs 100/--epochs $epochs/" commands_1.sh | sed -E 's/--WP "\S+"/--WP "WP.tmp.mat"/' > commands_2.sh
+    if [ -f "WT.tmp.mat" ]; then
+        sed -i -E 's/--WT "\S+"/--WT "WT.tmp.mat"/' commands_2.sh
+    fi
     chmod 755 commands_2.sh
     cd -
 done
