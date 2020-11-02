69bd70962fcca7055e8f51c82ce3fdf991de8b2e
degnbol <cdegma@dtu.dk>
2020-11-02 20:21:56 +0100

    also check substrates

--- iscience/gps/info.sh
+++ iscience/gps/info.sh
@@ -3,3 +3,6 @@ echo "norm is necessary since e.g. top 20% edges is all possible edges for some 
 cat gps_scores_max.tsv | mlr --tsv uniq -f kinase -c
 head -n 40000 gps_scores_max.tsv | mlr --tsv uniq -f kinase -c
 head -n 40000 gps_scores_max_norm.tsv | mlr --tsv uniq -f kinase -c
+# we don't have a problem with substrates it seems
+head -n 40000 gps_scores_max.tsv | mlr --tsv uniq -f substrate -c | wc -l
+head -n 40000 gps_scores_max_norm.tsv | mlr --tsv uniq -f substrate -c | wc -l
