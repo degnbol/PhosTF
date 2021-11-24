0343d18ee40b78dc584161fb84faae7c82478e32
Christian Madsen <christian.degnbol@gmail.com>
2021-11-24 14:08:45 +0100

    shebang

--- data/testdata/data_cas_neg/README.txt
+++
@@ -1,1 +0,0 @@
-added a negative PP->PK edge on data_cas to make it easier to notice cascades.
--- data/yeast_simulation/simulations/01/history.jl
+++ data/yeast_simulation/simulations/01/history.jl
@@ -1,3 +1,4 @@
+#!/usr/bin/env julia
 # in julia interactive. _cor files made with
 PKTFX.correct("WT_rand.mat")
 PKTFX.network("WT_cor.mat", "WP_cor.mat")
