448d7c1fc5a50890b7b719512ffafb8e88bd7769
Christian Madsen <christian.degnbol@gmail.com>
2021-11-25 16:37:56 +0100

    correct some of the paths

--- data/toy/simulation/commands.sh
+++ data/toy/simulation/commands.sh
@@ -1,6 +1,7 @@
-./simulation.cmd.jl
+#!/usr/bin/env zsh
+./simulate.cmd.jl
 for mutnum in {1..4}; do
 ~/cwd/src/plot_simulation.R 3 2 1 $mutnum
 done
-~/cwd/inference.jl X_sim.mat 2 3 --epochs 30000
+~/cwd/infer.jl X_sim.mat 2 3 --epochs 30000
 ./cytoscape.cmd.jl
