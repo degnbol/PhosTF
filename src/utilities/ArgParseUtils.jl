dd2b4e19337c3bf4d5a80a2724d9a7abab784cfe
Christian Madsen <christian.degnbol@gmail.com>
2021-11-26 12:54:31 +0100

    add argument types to be more readable and specific

--- src/utilities/ArgParseUtils.jl
+++ src/utilities/ArgParseUtils.jl
@@ -6,17 +6,18 @@ using NamedTupleTools
 
 export parse_arguments
 
-get_field_groups(settings) = [field.group for field in settings.args_table.fields]
-get_field_dest_names(settings) = [field.dest_name for field in settings.args_table.fields]
+get_field_groups(settings::ArgParseSettings) = [field.group for field in settings.args_table.fields]
+get_field_dest_names(settings::ArgParseSettings) = [field.dest_name for field in settings.args_table.fields]
 
 "Returns (positionals, optionals) as named tuples."
-function parse_arguments(settings)
+function parse_arguments(settings::ArgParseSettings)
     parsed_args = (;parse_args(settings; as_symbols=true)...)
     positional_keys = get_field_dest_names(settings)[get_field_groups(settings) .== "positional"]
     split(parsed_args, Tuple(Symbol.(positional_keys)))
 end
 
-function main(settings, f)
+"Run the main function f giving it parsed arguments, which are parsed according to the "
+function main(settings::ArgParseSettings, f)
     positionals, optionals = parse_arguments(settings)
     @info("Positionals: $positionals")
     @info("Optionals: $optionals")
