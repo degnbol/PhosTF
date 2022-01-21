9d5476e6cb5586e2ec932151da07372e3c940558
Christian Madsen <christian.degnbol@gmail.com>
2022-01-21 15:12:46 +0000

    note startup.jl potential

--- activate.sh
+++ activate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
-# set the julia project env variable if the dependencies were installed to their own environment with install.jl
+# set the julia project env variable if the dependencies were installed to their own environment with install.jl and you don't wish to add package auto-detection to your startup.jl. 
 # USAGE: . ./activate.sh (or `source /path/to/git/acticate.sh`)
 export JULIA_PROJECT=`git root`
