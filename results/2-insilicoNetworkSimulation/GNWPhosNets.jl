58347e615fb51cc0965384985f25c04fcdad8839
Christian Madsen <christian.degnbol@gmail.com>
2022-01-27 18:55:48 +0000

    header=true

--- results/2-insilicoNetworkSimulation/GNWPhosNets.jl
+++ results/2-insilicoNetworkSimulation/GNWPhosNets.jl
@@ -1,7 +1,6 @@
 #!/usr/bin/env julia
 # Run from `git root`/results/*-insilicoNetworkSimulation
-ROOT=readchomp(`git root`)
-include(ROOT * "/src/simulation/simulate.jl");
+include(readchomp(`git root`) * "/src/simulation/simulate.jl");
 
 mkpath("GNWPhosNets")
 
@@ -9,7 +8,7 @@ for n in [10, 100]
     for i in 1:5
         for rep in 1:5
             fnames = ["adjacencies/W$(s)_$(n)_$(i)-rep$(rep).adj" for s in "TP"]
-            network(fnames...; o="GNWPhosNets/net_$(n)_$(i)-rep$(rep).bson")
+            network(fnames...; header=true, o="GNWPhosNets/net_$(n)_$(i)-rep$(rep).bson")
         end
     end
 end
