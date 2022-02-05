449353f9e8b6e6c3836a9ecc1586fa550f3aff29
Christian Madsen <christian.degnbol@gmail.com>
2022-02-05 15:55:25 +0000

    print progress

--- results/2-insilicoNetworkSimulation/sim_logFCs.jl
+++ results/2-insilicoNetworkSimulation/sim_logFCs.jl
@@ -7,9 +7,10 @@ using .Iterators: product # since @threads does not support nested loops yet
 mkpath("sim_logFCs")
 
 println("beginning sim")
-for n in [10, 100]
+#= for n in [10, 100] =#
+for n in [10]
     @threads for (i, rep) in collect(product(1:5, 1:5))
-        println("sim $i $rep")
+        println("sim $n $i $rep")
         logFC("GNWPhosNets/net_$(n)_$(i)-rep$(rep).bson", "sim_logFCs/sim_logFC_$(n)_$(i)-rep$(rep).tsv");
     end
 end
--- results/3-insilicoNetworkInference/inferredWeights.jl
+++ results/3-insilicoNetworkInference/inferredWeights.jl
@@ -12,21 +12,21 @@ WT = nothing
 WP = nothing
 WT_mask = nothing
 WP_mask = nothing
-TF = r"^TF[0-9]+$"
-KP = r"^KP[0-9]+$"
+TF = r"^T[0-9]+$"
+KP = r"^P[0-9]+$"
 mut_sep = nothing
 
 logFC_dir = glob("../*-insilicoNetworkSimulation/sim_logFCs/") |> only
 #= logFC_fname = logFC_dir * "sim_logFC_100_1-rep1.tsv" =#
 #= infer(logFC_fname, TF, KP) =#
 
-for n in [10]
 #= for n in [10, 100] =#
+for n in [10]
     @threads for (i, rep) in collect(product(1:5, 1:5))
         SUF="_$(n)_$(i)-rep$(rep)"
         logFC_fname = logFC_dir * "sim_logFC$SUF.tsv"
         log_fname = "logs/log$SUF.tsv"
-        isfile(log_fname) || infer(logFC_fname, TF, KP, "inferredWeights/WT$SUF.adj", "inferredWeights/WP$SUF.adj"; log=log_fname, lambda_Bstar=1., lambda_absW=0.)
+        infer(logFC_fname, TF, KP, "inferredWeights/WT$SUF.adj", "inferredWeights/WP$SUF.adj"; log=log_fname, lambda_Bstar=1., lambda_absW=0.)
     end
 end
 
