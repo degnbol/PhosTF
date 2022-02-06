f5234d2d313f3f32b90fd37db5a7abbbc2688713
Christian Degnbol Madsen <madsen@maths.ox.ac.uk>
2022-02-06 16:36:00 +0000

    focus on P

--- results/hyper/print_best.sh
+++ results/hyper/print_best.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env zsh
-mlr --tsv --from aucs.tsv stats1 -a mean -f AUC -g cancel,mean_k,vec,norm,Bstar,absW then sort -nr AUC_mean | head
+mlr --tsv --from aucs.tsv filter '$source == "P"' then stats1 -a mean,min -f AUC -g cancel,mean_k,vec,norm,Bstar,absW then sort -nr AUC_mean,AUC_min | head
