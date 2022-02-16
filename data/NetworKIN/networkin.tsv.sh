10affb9808c79a729a8b12f50318085c44b60952
Christian Madsen <christian.degnbol@gmail.com>
2022-02-16 18:10:05 +0000

    better paths

--- data/NetworKIN/networkin.tsv.sh
+++ data/NetworKIN/networkin.tsv.sh
@@ -1,6 +1,7 @@
+#!/usr/bin/env zsh
 # running all against all
 sed 1d ../biogrid/PTM.res > PTM.res
-../../../apps/NetworKIN_v3/NetworKIN ../SGD/orfs.aa > networkin.tsv
-../../../apps/NetworKIN_v3/NetworKIN ../SGD/orfs.aa PTM.res > networkin_biogrid.tsv
+`git root`/etc/NetworKIN_v3/NetworKIN ../SGD/orfs.aa > networkin.tsv
+`git root`/etc/NetworKIN_v3/NetworKIN ../SGD/orfs.aa PTM.res > networkin_biogrid.tsv
 # reduce the heavy file by removing text description columns.
-cat networkin.tsv | cut -d$'\t' -f1-2,4-12,15-16 > temp && mv temp networkin.tsv
+cat networkin.tsv | cut -f1-2,4-12,15-16 > temp && mv temp networkin.tsv
