2a387026d5ec9a37d6880fc527c1ad1b976049a8
Christian Madsen <christian.degnbol@gmail.com>
2022-02-16 18:06:55 +0000

    gunzip

--- data/SGD/raw.sh
+++ data/SGD/raw.sh
@@ -5,3 +5,5 @@ cd raw
 wget 'http://downloads.yeastgenome.org/sequence/S288C_reference/orf_protein/orf_trans_all.fasta.gz'
 # gene associations downloaded from http://downloads.yeastgenome.org/?prefix=curation/literature/
 wget 'http://downloads.yeastgenome.org/curation/literature/gene_association.sgd.gaf.gz'
+gunzip *.gz
+cd -
