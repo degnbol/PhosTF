13ecb316634d4262be70bd6abe7cc9c2e434e6bd
Christian Degnbol Madsen <madsen@maths.ox.ac.uk>
2022-02-19 23:16:31 +0000

    update pckgs

--- results/4-yeastNetworkReconstuction/infer/infers.sh
+++ results/4-yeastNetworkReconstuction/infer/infers.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env zsh
 for iORo in "inner" "outer"; do
-    for logFC in "" "-strict" "-enh1" "-enh4"; do
-        for lB in "0.0" "0.01" "0.1" "0.5" "1.0"; do
-            for lW in "0.0" "0.01" "0.1" "0.5" "1.0"; do
-                julia -t 10 ./infer.jl $iORo $logFC $lB $lW &
+    for logFC in "-strict" "-enh1" "-enh4"; do
+        for lB in "0.0" "0.01" "0.1" "1.0"; do
+            for lW in "0.0" "0.01" "0.1" "1.0"; do
+                ./infer.jl $iORo $logFC $lB $lW > log$iORo$logFC-B$lB-W$lW.tsv &
             done
         done
     done
