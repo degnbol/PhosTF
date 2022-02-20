060a007f2da389e9ef3d4b1fe916d5792184f384
Christian Degnbol Madsen <madsen@maths.ox.ac.uk>
2022-02-20 12:10:36 +0000

    WT test

--- results/4-yeastNetworkReconstuction/infer/infer.jl
+++ results/4-yeastNetworkReconstuction/infer/infer.jl
@@ -1,9 +1,25 @@
 #!/usr/bin/env julia
 @src "inference/infer"
 
-iORo, logFC, λB, λW = [v for ARG in ARGS for v in split(ARG, '_')]
+iORo, logFC, λB, λW, WT, WT_mask = [v for ARG in ARGS for v in split(ARG, '+')]
+
 
 suff = "$iORo$logFC"
+if WT == ""
+    WT = nothing
+else
+    WT = "../WT_$(WT).ssv"
+end
+if WT_mask == ""
+    WT_mask = nothing
+else
+    WT_mask = "../WT_$(WT_mask).ssv"
+end
 infer("../logFC_$suff.csv", "../TF.txt", "../KP.txt", "WT_infer-$suff-$λB-$λW.tsv", "WP_infer-$suff-$λB-$λW.tsv"; 
-delim_mut='_', col_match=r"^[\w-]+", lambda_Bstar=parse(Float64, λB), lambda_absW=parse(Float64, λW), epochs=10)
+delim_mut='_', col_match=r"^[\w-]+",
+lambda_Bstar=parse(Float64, λB),
+lambda_absW=parse(Float64, λW),
+epochs=200,
+WT=WT, train_WT=WT===nothing,
+WT_mask=WT_mask)
 
