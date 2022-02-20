e3d761b70a1f9e03a58c729589f79842560a009f
Christian Madsen <christian.degnbol@gmail.com>
2022-02-20 12:46:23 +0000

    subtable func tested

--- src/utilities/DataFrameUtils.jl
+++ src/utilities/DataFrameUtils.jl
@@ -1,9 +1,18 @@
 using DataFrames
 
-# rearranges, does not convert to Matrix of remove row column
-subtable(df::DataFrame, colnames, rownames) = rowselect(df[:, colnames], rownames)
+"""
+rearranges, does not convert to Matrix of remove row column.
+Assumes first column contains rownames.
+"""
+function subtable(df::DataFrame, colnames, rownames)
+    rowcol = names(df)[1]
+    @assert rowcol ∉ colnames "Rownames should be in first col named $rowcol, which should not be listed among colnames."
+    rowselect(df, rownames)[:, [rowcol; colnames]]
+end
 
-# rearranges, does not convert to Matrix of remove row column
+"""
+rearranges, does not convert to Matrix of remove row column
+"""
 function rowselect(df::DataFrame, rownames)
     # make sure there are no replicate entries in rownames with the only function
     rowind = [only(findall(df[!, 1] .== k)) for k in rownames]
