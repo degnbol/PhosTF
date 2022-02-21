b49557699a2821390c63053a28526b90d0a23be1
Christian Madsen <christian.degnbol@gmail.com>
2022-02-21 17:06:26 +0000

    fasolo streamlined

--- data/fasolo_2011/P_edges_ORF.tsv.sh
+++ data/fasolo_2011/P_edges_ORF.tsv.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
-# correct sys names by inserting a -
-sed -E 's/(Y[A-Z]{2}[0-9]{3}[A-Z])([A-Z])/\1-\2/' P_edges_ORF.tsv > temp && mv temp P_edges_ORF.tsv
+# correct systematic names by inserting a dash, e.g. "YHR214CC" -> "YHR214C-C"
+sed -E 's/(Y[A-Z]{2}[0-9]{3}[A-Z])([A-Z])/\1-\2/' kinase_edges.tsv > P_edges_ORF.tsv
---
+++ data/fasolo_2011/kinase_edges.tsv.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# REQUIRES: raw/FasoloSuppTableS1.pdf.tsv
+# WRITES: kinase_edges.tsv
+
+kinases = []
+targets = []
+
+with open("raw/FasoloSuppTableS1.pdf.tsv") as fh:
+    # first line contains kinases
+    ks = next(fh).strip().split('\t')
+    print("kinases:", ks)
+    
+    for line in fh:
+        if line.strip(" \t\r\n") == "":
+            # new table, new kinases
+            ks = next(fh).strip().split('\t')
+            print("kinases:", ks)
+        else:
+            ts = line.strip().split('\t')
+            assert len(ks) >= len(ts), f"len({ks}) < len({ts})"
+            kinases.extend(ks[:len(ts)]) # the row may not have as many elements as the header
+            targets.extend(ts) # some entries will be ""
+
+with open("kinase_edges.tsv", 'w') as fh:
+    fh.write("kinase\ttarget\n")
+    for kinase, target in zip(kinases, targets):
+        if target != "":
+            fh.write(f"{kinase}\t{target}\n")
+
+
