cd2226511cf8043a2ef6805256a925c2cfde4cb7
Christian Madsen <christian.degnbol@gmail.com>
2022-02-21 20:06:08 +0000

    2ORF

--- data/fasolo_2011/P_edges_ORF.tsv.sh
+++ data/fasolo_2011/P_edges_ORF.tsv.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env zsh
 # correct systematic names by inserting a dash, e.g. "YHR214CC" -> "YHR214C-C"
-sed -E 's/(Y[A-Z]{2}[0-9]{3}[A-Z])([A-Z])/\1-\2/' kinase_edges.tsv > P_edges_ORF.tsv
+sed -E 's/(Y[A-Z]{2}[0-9]{3}[A-Z])([A-Z])/\1-\2/' kinase_edges.tsv > kinase_edges-.tsv
+../gene2ORF/gene2ORF.R kinase_edges-.tsv P_edges_ORF.tsv ../gene2ORF/gene2ORF.tsv
+rm kinase_edges-.tsv
+
