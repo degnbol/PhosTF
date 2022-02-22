df9d121840646928ff20de394319339efe702971
Christian Madsen <christian.degnbol@gmail.com>
2022-02-22 19:01:30 +0000

    install and require

--- results/4-yeastNetworkReconstuction/P_edges/P_edges_mode.tsv.sh
+++ results/4-yeastNetworkReconstuction/P_edges/P_edges_mode.tsv.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env zsh
+# REQUIRES: P_data-biogrid.tsv and P_edges.tsv
+# INSTALL: merge.R from github.com/degnbol/degnlib
 # annotate P_edges.tsv with the kinase or phosphatase action from biogrid
 merge.R -H P_data-biogrid.tsv < P_edges.tsv | 
     mlr --tsv cut -f P,Target,Value then rename Value,Mode > P_edges_mode.tsv
