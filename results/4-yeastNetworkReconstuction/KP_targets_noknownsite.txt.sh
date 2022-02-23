7605ac5a2df3b0462d4c5fd3693179707800b6ba
Christian Madsen <christian.degnbol@gmail.com>
2022-02-23 21:28:41 +0000

    sort | uniq -> sort -u

--- results/4-yeastNetworkReconstuction/KP_targets_noknownsite.txt.sh
+++ results/4-yeastNetworkReconstuction/KP_targets_noknownsite.txt.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 # REQUIRES: KP_targets.txt and `git root`/data/biogrid/targets.txt
-comm -23 <(sort KP_targets.txt | uniq) <(sort `git root`/data/biogrid/targets.txt | uniq) > KP_targets_noknownsite.txt
+comm -23 <(sort -u KP_targets.txt) <(sort -u `git root`/data/biogrid/targets.txt) > KP_targets_noknownsite.txt
