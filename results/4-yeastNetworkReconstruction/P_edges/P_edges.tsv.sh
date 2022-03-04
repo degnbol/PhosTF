bc4fcabff849fbf0f8957f525be20e63acd82b25
Christian Madsen <christian.degnbol@gmail.com>
2022-03-04 18:14:12 +0000

    no self edges and make invitro file

--- results/4-yeastNetworkReconstruction/P_edges/P_edges.tsv.sh
+++ results/4-yeastNetworkReconstruction/P_edges/P_edges.tsv.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env zsh
+# no self edges allowed.
 # yeastkid Score > 4.52 corresponds to p-value < 0.05
 # ptmod scores are in range 0 to 1000 where higher is better. #uniq edges:
 # ptmod > 900 -> 0
@@ -8,8 +9,10 @@
 # ptmod > 500 -> 226
 mlr --tsv --from P_data.tsv filter \
    '($Variable == "Score" && $Value > 4.52) || 
-    ($Variable == "ptmod" && $Value > 800)  || 
-    ($Variable != "Score" && $Variable != "ptmod")' then \
-    uniq -f P,Target then filter '$P != $Target' > P_edges.tsv
-# no self edges allowed
+    ($Variable == "ptmod" && $Value > 500)  || 
+    ($Variable != "Score" && $Variable != "ptmod")' then\
+    uniq -f P,Target,Ref then filter '$P != $Target' > P_edges.tsv
+
+mlr --tsv --from P_edges.tsv filter '$Ref != "Ptacek 2005"' then\
+    uniq -f P,Target > P_edges_invivo.tsv
 
