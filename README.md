02c57a8cc45919152a493d884914bfb11ba314a3
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-05-02 16:04:15 +1000

    version problems with DifferentialEquations

--- README.md
+++ README.md
@@ -2,12 +2,11 @@
 Simulation and inference of networks of gene regulation with Protein Kinase, Transcription Factor and gene elements. Most functions can be called through simulation.jl and inference.jl.
 
 ## REQUIRMENTS
-- Julia. Currently on 1.7.1
-
-## CONFIG
-- Configure a `git root` command by running `./config.sh`.
+- Julia. Currently on 1.7.2
 
 ## INSTALL
-- Julia dependencies can be installed with `./install.jl`.
+- Run `./install.sh` which sets command `git root`
+- Either run `./install.jl` or `./install_instantiate.jl` which installs Julia 
+  dependencies as newest versions or as tested versions, respectively.
 - Auto-detect environment at startup by adding `startup.jl` to `~/.julia/config/startup.jl`, e.g. by running `cat startup.jl >> ~/.julia/config/startup.jl`
 
--- install.jl
+++ install.jl
@@ -1,4 +1,51 @@
 #!/usr/bin/env julia
+# First remove record of tested versions to do a clean install.
+rm("Project.toml")
+rm("Manifest.toml")
+
 using Pkg
 Pkg.activate(".")
-Pkg.instantiate()
+
+Pkg.add("DifferentialEquations")
+Pkg.add("Flux")
+
+# precompile major packages to make sure they work before running jobs.
+using Flux, DifferentialEquations
+
+Pkg.add([
+"ArgParse",
+"BSON",
+"CSV",
+"Chain",
+"Colors",
+"DataFrames",
+"Dates",
+"DelimitedFiles",
+"DiffEqCallbacks",
+"Distributions",
+"Fire",
+"Formatting",
+"Glob",
+"HDF5",
+"InlineStrings",
+"IterTools",
+"JLD",
+"JSON3",
+"LanguageServer",
+"LinearAlgebra",
+"Logging",
+"NamedTupleTools",
+"Random",
+"Revise",
+"SparseArrays",
+"Statistics",
+"StatsBase",
+"Test",
+])
+
+Pkg.add(url="https://github.com/diegozea/ROC.jl")
+Pkg.add(url="https://github.com/mortenpi/ProjectX.jl.git")
+
+# Using ProjectX we can have project local startup code auto loaded.
+run(`cat Project.toml.projectx >> ./Project.toml`)
+
