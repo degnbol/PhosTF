421f9a9781b1e19949c916473b8475ee2f3fdc9d
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-05-05 19:42:39 +1000

    example for review comment showing effect of varying λB*

--- results/1-smallExamples/2-4types/regTests/commands.jl
+++ results/1-smallExamples/2-4types/regTests/commands.jl
@@ -10,8 +10,8 @@ for λBstar in [1., .1, .01, .001, 0.]
         WT_infer = "WT_inferIdeal-B$(λBstar)-W$(λabsW).tsv"
         WP_infer = "WP_inferIdeal-B$(λBstar)-W$(λabsW).tsv"
         cy_infer =    "inferIdeal-B$(λBstar)-W$(λabsW).xgmml"
-        infer("../ideal_logFC.tsv", r"T.*", r"P.*", WT_infer, WP_infer; epochs=20000, lambda_Bstar=λBstar, lambda_absW=λabsW)
-        xgmml(WT_infer, WP_infer; o=cy_infer)
+        # infer("../ideal_logFC.tsv", r"T.*", r"P.*", WT_infer, WP_infer; epochs=20000, lambda_Bstar=λBstar, lambda_absW=λabsW)
+        xgmml(WT_infer, WP_infer; o=cy_infer, typecol=false, showlab=false)
     end
 end
 
@@ -27,13 +27,15 @@ for sim in 1:5
     network(WT, WP; header=true, o=net)
     logFC(net, "sim$(sim)_logFC.tsv")
     
-    for λBstar in [1., .1, .01, .001, 0.]
-        for λabsW in [1., .1, .01, .001, 0.]
+    # for λBstar in [1., .1, .01, .001, 0.]
+    for λBstar in [10., 1., .1, .01, 0.]
+        # for λabsW in [1., .1, .01, .001, 0.]
+        for λabsW in [0.]
             WT_infer = "WT_infer$(sim)-B$(λBstar)-W$(λabsW).tsv"
             WP_infer = "WP_infer$(sim)-B$(λBstar)-W$(λabsW).tsv"
             cy_infer =    "infer$(sim)-B$(λBstar)-W$(λabsW).xgmml"
             infer("sim$(sim)_logFC.tsv", r"T.*", r"P.*", WT_infer, WP_infer; epochs=20000, lambda_Bstar=λBstar, lambda_absW=λabsW)
-            xgmml(WT_infer, WP_infer; o=cy_infer)
+            xgmml(WT_infer, WP_infer; o=cy_infer, typecol=false, showlab=false)
         end
     end
 end
--- results/1-smallExamples/2-4types/regTests/inferIdeal.pdfs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zsh
-for file in infer*.xgmml; do
-    [ -f $file:r.pdf ] && rm $file:r.pdf
-    /Users/degnbol/PhosTF/src/scripts/xgmml2pdf.py $file $file:r
-done
