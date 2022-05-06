09ac69d003ef3b18ddc14f8c4726a278cbdf7dd7
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-05-06 14:53:20 +1000

    more dream files in git etc

--- results/hyper/print_best.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env zsh
-mlr --tsv --from aucs.tsv filter '$source == "P"' then stats1 -a mean,min -f AUC -g cancel,mean_k,vec,norm,Bstar,absW then sort -nr AUC_mean,AUC_min | head
--- results/hyper/see_best.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env zsh
-./print_best.sh | head -n2 | mlr --tsv join -f aucs.tsv -j cancel,mean_k,vec,norm,Bstar,absW then sort -n AUC
