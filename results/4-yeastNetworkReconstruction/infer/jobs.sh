71c165e0edba0ac88c6b57525acee61622e782f7
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-05-17 18:37:11 +1000

    sort

--- results/4-yeastNetworkReconstruction/infer/jobs.sh
+++ results/4-yeastNetworkReconstruction/infer/jobs.sh
@@ -1,23 +1,31 @@
 #!/usr/bin/env zsh
 
-# 'inner+-strict+1.0+0.0+FDR20+'
+# 'inner+-strict+1.0+0.0+FDR20+' \
+# 'inner+-strict+0.1+0.0+FDR20+' \
+# 'inner+-strict+0.0+1.0+FDR20+' \
+# 'inner+-strict+0.0+0.1+FDR20+' \
+# 'outer+-strict+1.0+0.0+FDR20+' \
+# 'outer+-strict+0.1+0.0+FDR20+' \
+# 'outer+-strict+0.0+1.0+FDR20+' \
+# 'outer+-strict+0.0+0.1+FDR20+' \
+# 'inner+-strict+1.0+0.0+FDR20+FDR20_abssign_mask' \
+# 'inner+-strict+0.1+0.0+FDR20+FDR20_abssign_mask' \
+# 'inner+-strict+0.0+1.0+FDR20+FDR20_abssign_mask' \
+# 'inner+-strict+0.0+0.1+FDR20+FDR20_abssign_mask' \
+# 'outer+-strict+1.0+0.0+FDR20+FDR20_abssign_mask' \
+# 'outer+-strict+0.1+0.0+FDR20+FDR20_abssign_mask' \
+# 'outer+-strict+0.0+1.0+FDR20+FDR20_abssign_mask' \
+# 'outer+-strict+0.0+0.1+FDR20+FDR20_abssign_mask'
 
 for params in \
-'inner+-strict+0.1+0.0+FDR20+' \
-'inner+-strict+0.0+1.0+FDR20+' \
-'inner+-strict+0.0+0.1+FDR20+' \
-'outer+-strict+1.0+0.0+FDR20+' \
-'outer+-strict+0.1+0.0+FDR20+' \
-'outer+-strict+0.0+1.0+FDR20+' \
-'outer+-strict+0.0+0.1+FDR20+' \
-'inner+-strict+1.0+0.0+FDR20+FDR20_abssign_mask' \
-'inner+-strict+0.1+0.0+FDR20+FDR20_abssign_mask' \
-'inner+-strict+0.0+1.0+FDR20+FDR20_abssign_mask' \
-'inner+-strict+0.0+0.1+FDR20+FDR20_abssign_mask' \
-'outer+-strict+1.0+0.0+FDR20+FDR20_abssign_mask' \
-'outer+-strict+0.1+0.0+FDR20+FDR20_abssign_mask' \
-'outer+-strict+0.0+1.0+FDR20+FDR20_abssign_mask' \
-'outer+-strict+0.0+0.1+FDR20+FDR20_abssign_mask'
+'inner+-strict+1.0+0.0++FDR20_abssign_mask' \
+'inner+-strict+0.1+0.0++FDR20_abssign_mask' \
+'inner+-strict+0.0+1.0++FDR20_abssign_mask' \
+'inner+-strict+0.0+0.1++FDR20_abssign_mask' \
+'outer+-strict+1.0+0.0++FDR20_abssign_mask' \
+'outer+-strict+0.1+0.0++FDR20_abssign_mask' \
+'outer+-strict+0.0+1.0++FDR20_abssign_mask' \
+'outer+-strict+0.0+0.1++FDR20_abssign_mask'
 do
 echo "#!/usr/bin/env bash
 #SBATCH -N 1
@@ -36,4 +44,5 @@ module load r/4.1.0
 time ./infer.sh '$params'
 " > job-$params.slurm
 sbatch job-$params.slurm
+mv job-$params.slurm jobs/$params.slurm
 done
--- results/4-yeastNetworkReconstruction/infer/pprint.sh
+++ results/4-yeastNetworkReconstruction/infer/pprint.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env zsh
-tr '+' '\t' < hyperps.tsv | sed 's/.tsv//' | sed 's/WP_infer-//' | cat <(echo "DF_size\tDF_issues\tB*\t|W|\tWT0\tWT_mask\tp") - | mlr --itsv --opprint cat
+tr '+' '\t' < hyperps.tsv |
+    sed 's/.tsv//' | sed 's/WP_infer-//' | sed 's/ $//' |
+    cat <(echo "DF_size\tDF_issues\tB*\t|W|\tWT0\tWT_mask\tp") - |
+    mlr --itsv --opprint sort -n p
