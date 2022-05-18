d7597db3553daa114fc1e7c9d3297b4cc8c663c1
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-05-18 11:59:48 +1000

    r -> dupl action

--- results/4-yeastNetworkReconstruction/logFC_norms.csv.gz.sh
+++ results/4-yeastNetworkReconstruction/logFC_norms.csv.gz.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 ./norm.R logFC_inner-strict.csv | sed '/1/s/""//' | gzip > logFC_inner-strict_norm.csv.gz
-r inner=outer
+./norm.R logFC_outer-strict.csv | sed '/1/s/""//' | gzip > logFC_outer-strict_norm.csv.gz
